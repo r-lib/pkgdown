c80544298b3724fc7fca659c55aa3910ea31c2ff
hadley <h.wickham@gmail.com>
2012-03-15 12:23:31 -0500

    Generate a package index

--- R/package.r
+++ R/package.r
@@ -1,8 +1,12 @@
 build_package <- function(package, base_path) {
   if (!file.exists(base_path)) dir.create(base_path)
+  copy_bootstrap(base_path)
+  
+  info <- package_info(package)
+  info$vignettes <- build_vignettes(package, base_path)
+  render_template("index", info, file.path(base_path, "index.html"))
   
   build_topics(package, base_path)
-  copy_bootstrap(base_path)
   
   invisible(TRUE)
 }
@@ -18,9 +22,9 @@ build_topics <- function(package, base_path) {
   html <- lapply(rd, to_html)
   
   files <- index$file[!duplicated(index$file)]
-  paths <- file.path(base_path, str_c(files, ".html"))
+  paths <- file.path(base_path, files)
 
-  mapply(render_topic, html, paths)
+  mapply(render_template, "topic", html, paths)
 }
 
 copy_bootstrap <- function(base_path) {
@@ -36,181 +40,65 @@ topic_index <- function(package) {
     stringsAsFactors = FALSE, comment.char = "", quote = "", header = FALSE)
     
   names(topics) <- c("alias", "file") 
+  topics$file <- str_c(topics$file, ".html")
   topics[complete.cases(topics), ]
 }
 
 
-
-#' Package information.
-#'
-#' @aliases helpr_package helpr_package_mem
-#' @return all the information necessary to produce a package site ("/package/:package/")
-helpr_package <- function(package) {
-  helpr_package_mem(package, pkg_version(package))
-}
-
-
-
-helpr_package_mem <- function(package, version) {
+package_info <- function(package) {
   
   info <- readRDS(system.file("Meta", "package.rds", package = package))
-  description <- as.list(info$DESCRIPTION)
-  info$DESCRIPTION <- NULL
-  description <- modifyList(info, description)
-  names(description) <- tolower(names(description))
+  desc <- as.list(info$DESCRIPTION)
   
-  author_str <- pluralize("Author", description$author)
+  out <- list()
   
-  items <- pkg_topics_alias(package)
+  out$package <- desc$Package
+  out$version <- desc$Version
+  out$description <- desc$Description
   
-  demos <- pkg_demos(package)
-  vigs <- pkg_vigs(package)
-
-  description$depends <- parse_pkg_desc_item(description$depends)
-  description$imports <- parse_pkg_desc_item(description$imports)
-  description$suggests <- parse_pkg_desc_item(description$suggests)
-  description$extends <- parse_pkg_desc_item(description$extends)
-  description$reverse <- dependsOnPkgs(package)
-  description$author <- pkg_author_and_maintainers(description$author, description$maintainer)
-  description$maintainer <- NULL
-
-  if (has_text(description$url)) {
-    description$url <- str_trim(str_split(description$url, ",")[[1]])
+  out$authors <- desc$Author
+  out$maintainer <- desc$Maintainer
+  if (!is.null(desc$url)) {
+    out$urls <- str_trim(str_split(desc$url, ",")[[1]])    
   }
 
-  list(
-    package = package, 
-    items = items,
-    description = description, 
-    author_str = author_str, 
-    demos_str = pluralize("Demo", demos),
-    demos = demos,
-    vigs_str = pluralize("Vignette", vigs),
-    vigs = vigs,
-    change_log = pkg_news(package)
+  # Dependencies 
+  pkg_names <- function(x) vapply(x, "[[", "name", FUN.VALUE = character(1))
+  out$dependencies <- list(
+    depends = pkg_names(info$depends),
+    imports = pkg_names(info$imports),
+    suggests = pkg_names(info$suggests),
+    extends = pkg_names(info$extends)
   )
-}
 
-#' Package version from the rd file
-#'
-#' @keywords internal
-#' @author Hadley Wickham
-pkg_version <- function(pkg) {
-  rds <- read_rds(system.file("Meta", "package.rds", package = pkg))
-  rds$DESCRIPTION[["Version"]]
-}
+  # Topics
+  index <- topic_index(package)
+  out$topics <- unname(apply(index, 1, as.list))
 
-
-#' Add package link to a string.
-#' 
-#' @param string string to be evaluated
-#' @keywords internal
-#' @author Barret Schloerke \email{schloerke@@gmail.com}
-#' @examples #add_package_link_to_string("quantreg, Hmisc, mapproj, maps, hexbin, gpclib, maptools")
-add_package_link_to_string <- function(string) {
-  packs <- str_trim(str_split(usage_functions(string), "[, ]")[[1]])
-  packs <- packs[packs != ""]
-  pack_link <- str_c("<a href='", router_url(), "/package/", packs, "/' >", packs, "</a>")
-
-  for(i in seq_along(packs)){
-    string <- str_replace(string, packs[i], pack_link[i])[[1]]
-  }
-  string
-}
-
-#' Ensure package version is properly displayed (if not already in a nice
-#' format).
-#'
-parse_pkg_desc_item <- function(obj) {
-  if (NROW(obj) < 1) {
-    return(NULL)
-  }
-  
-  if (is.character(obj)) {
-    return(obj)
-  }
-  
-  if (!is.list(obj)) {
-    obj <- list(obj = (list(name = obj, version = NULL)))
-  }
-
-  as.data.frame(
-    sapply(obj, function(x) {
-      vers <- NULL
-      
-      # if the version is found, it will create one in the form of '(1.2.3)'
-      if (!is.null(x$version)) {
-        vers <- str_c("(", x$op, " ", str_c(unclass(x$version)[[1]], collapse = "."), ")", collapse = "")
-      }
-      list(name = as.character(x$name), version = as.character(vers))
-    })
-    , stringsAsFactors = FALSE
-  )
+  out
 }
 
 
 #' List all package vignettes.
 #'
+#' Copies all vignettes and returns data structure suitable for use with
+#' whisker templates.
+#'
 #' @param package package to explore
-#' @return \code{subset} of the \code{vignette()$results} \code{data.frame} ("Package", "LibPath", "Item" and "Title")
-pkg_vigs <- function(package) {
-  vignettes <- vignette(package = package)$results
+#' @return a list, with one element for each vignette containing the vignette
+#'   title and file name.
+build_vignettes <- function(package, base_path) {
+  vignettes <- as.data.frame(vignette(package = package)$results)
   
-  if (!NROW(vignettes)) {
-    return(NULL)
-  }
-
-  titles <- str_replace_all(vignettes[,4], "source, pdf", "")
-  titles <- str_trim(str_replace_all(titles, "[()]", ""))
-  
-  data.frame(item = vignettes[,"Item"], title = titles, stringsAsFactors = FALSE)
-}
-
-
-
-
-
-#' Topic title and aliases by package.
-#' return information on the package, datasets, internal, and datasets
-#'
-#' @param pkg package in question
-pkg_topics_alias <- function(pkg) {
+  title <- str_replace_all(vignettes$Title, " \\(source, pdf\\)", "")
+  filename <- str_c(vignettes$Item, ".pdf")
+  src <- file.path(vignettes$LibPath, vignettes$Package, "doc", filename)
     
-  rd1 <- pkg_topics_rd(pkg)
-  rd <- lapply(rd1, function(x) {
-    desc <- to_html(untag(x$description), pkg)
-    desc_naked <- strip_html(desc)
-    if (str_length(desc_naked) > 150) {
-      desc <- str_c(str_sub(desc_naked, end = 150), " ...")
-    }
-
-    list(
-      topic = unlist(x$name),
-      alias = unname(sapply(x[names(x) == "alias"], "[[", 1)),
-      keywords = str_trim(to_html(untag(x$keyword), pkg)),
-      desc = desc,
-      title = to_html(untag(x$title), pkg)
-    )
-  })
-
-  keywords <- sapply(rd, function(x){ x$keywords })
-          
-  package_info <- rd[keywords == "package"]
-  internal <- rd[keywords == "internal"]
-  dataset <- rd[keywords == "datasets"]  
+  dest <- file.path(base_path, "vignettes")
+  if (!file.exists(dest)) dir.create(dest)
   
-  rows <- keywords %in% c("package", "internal", "datasets")
-
-  if (sum(rows) > 0) rd[rows] <- NULL 
+  file.copy(src, file.path(dest, filename))
   
-  list(func = rd, dataset = dataset, internal = internal, info = package_info)
-}
-
-#' Package description
-#'
-#' @param pkg package in question
-#' @param topic topic in question
-package_description <- function(pkg, topic) {
-  gsub("$\n+|\n+^", "", to_html(pkg_topic(pkg, topic)$description, package))
+  apply(cbind(src, filename), 1, as.list)
 }
 
--- R/render.r
+++ R/render.r
@@ -3,17 +3,19 @@ find_template <- function(name) {
   file.path(inst_path(), "templates", str_c(name, ".html"))
 }
 
-#' Render topic html page.
+#' Render template.
 #' 
-#' @param data Data for the template - should usually be created by calling
-#'   \code{\link{to_html}} on the result of \code{\link{parse_rd}}
+#' @param name of the template
+#' @param data data for the template
+#' @param path location to create file.  If \code{""} (the default), 
+#'   prints to standard out.
 #' @importFrom whisker whisker.render
 #' @examples
 #' rd <- parse_rd("colSums", "base")
 #' html <- to_html(rd)
 #' render_topic(html)
-render_topic <- function(data, path = "") {
-  template <- readLines(find_template("topic"))
+render_template <- function(name, data, path = "") {
+  template <- readLines(find_template(name))
   
   rendered <- whisker.render(template, data)
   cat(rendered, file = path)
