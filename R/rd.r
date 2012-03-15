2d8c7285cd3527ad68fc10e607d09217c0637774
hadley <h.wickham@gmail.com>
2012-03-14 19:35:35 -0500

    Pretty printing. And fix class assignment error

--- R/rd.r
+++ R/rd.r
@@ -5,7 +5,8 @@ parse_rd <- function(topic, package) {
     list(topic = as.name(topic), package = as.name(package)))
   
   rd_path <- eval(help_call)[[1]]
-  set_classes(utils:::.getHelpFile(rd_path))
+  rd <- utils:::.getHelpFile(rd_path)
+  set_classes(rd)
 }
 
 
@@ -24,7 +25,10 @@ set_class <- function(x) {
 # Recursively set classes of Rd objects
 set_classes <- function(Rd) {
   if (is.list(Rd)) {
-    lapply(Rd, set_class)
+    new_Rd <- set_class(lapply(Rd, set_classes))
+    attr(new_Rd, "Rd_tag") <- attr(Rd, "Rd_tag")
+    attr(new_Rd, "Rd_option") <- attr(Rd, "Rd_option")
+    new_Rd
   } else {
     set_class(Rd)
   }
@@ -37,4 +41,22 @@ as.list.Rd <- function(x) {
   x
 }
 
+print.Rd <- function(x, ..., indent = 0) {
+  cat(str_dup(" ", indent), "\\- ", colourise(tag(x), "blue"), 
+    " (", length(x), ")\n", sep = "")
+  
+  lapply(x, print, indent = indent + 2)    
+}
 
+print.TEXT <- function(x, ..., indent = 0) block(x, indent, '"')
+print.VERB <- function(x, ..., indent = 0) block(x, indent, "'")
+print.RCODE <- function(x, ..., indent = 0) block(x, indent, ">")
+
+#' @importFrom testthat colourise
+block <- function(x, indent = 0, prefix = "'") {
+  x <- str_trim(x)
+
+  start <- str_c(str_dup(" ", indent), colourise(prefix, "blue"), " ")
+  cat(start, str_sub(x, 0, getOption("width") - str_length(start)), "\n", 
+    sep = "")  
+}
