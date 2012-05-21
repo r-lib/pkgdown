299b715ab1bc810bbc5123dd64b7f73cda79d933
hadley <h.wickham@gmail.com>
2012-05-21 17:46:38 -0500

    Fix thinko

--- R/util.r
+++ R/util.r
@@ -6,8 +6,10 @@ inst_path <- function() {
     system.file(package = "staticdocs")
   } else {
     # Probably in development
-    file.path(dirname(dirname(attr(srcref, "srcfile")$filename)),
-      "inst")
+    browser()
+    srcref <- attr(find_template, "srcref")
+    path <- dirname(dirname(attr(srcref, "srcfile")$filename))
+    file.path(path, "inst")
   }
 }
 
