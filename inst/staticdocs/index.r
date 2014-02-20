2c758b53f9fdea62092323398d7948ced7784373
hadley <h.wickham@gmail.com>
2014-02-20 14:13:01 -0600

    Index lives in statitic/docs/index.r

--- inst/staticdocs/index.r
+++ inst/staticdocs/index.r
@@ -1,13 +1,13 @@
 list(
   index = list(
     sd_section("Settings functions",
-      "These functions are used in `staticdocs.r` to configure various
+      "These functions are used in `index.r` to configure various
        settings that staticdocs uses when rendering a package.  This is
        particularly useful for generating an index page that groups functions
        into useful categories",
       c(
-        "sd_icon", 
-        "sd_item", 
+        "sd_icon",
+        "sd_item",
         "sd_section"
       )
     )
