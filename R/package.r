e4512d198c2ca929788844d474bf2ed3d543788b
Joe Cheng <joe@rstudio.com>
2014-03-17 14:43:20 -0700

    Restore template_path parameter

--- R/package.r
+++ R/package.r
@@ -7,11 +7,17 @@
 #' @param examples include examples or not? The default, \code{NULL}, first
 #'   looks at the value of \code{examples} set in \file{DESCRIPTION}, and if
 #'   not found uses \code{TRUE}.
+#' @param templates_path a specific directory path to use when searching for
+#'   rendering templates, in addition to the default locations of
+#'   packagedir/inst/staticdocs, packagedir/staticdocs, and the staticdocs
+#'   package's included templates directory.
 #' @return A named list of useful metadata about a package
 #' @export
 #' @keywords internal
 #' @importFrom devtools parse_deps as.package
-as.sd_package <- function(pkg = ".", site_path = NULL, examples = NULL) {
+as.sd_package <- function(pkg = ".", site_path = NULL, examples = NULL,
+  templates_path = NULL) {
+
   if (is.sd_package(pkg)) return(pkg)
 
   pkg <- as.package(pkg)
@@ -24,6 +30,7 @@ as.sd_package <- function(pkg = ".", site_path = NULL, examples = NULL) {
   settings <- load_settings(pkg)
   pkg$site_path <- site_path %||% settings$site_path %||% "inst/web"
   pkg$examples <- examples %||% settings$examples %||% TRUE
+  pkg$templates_path <- templates_path %||% settings$templates_path %||% NULL
 
   if (!is.null(pkg$url)) {
     pkg$urls <- str_trim(str_split(pkg$url, ",")[[1]])
