6cf620dd6af45fd750d99a183d6839233e9b2d3c
adrtod <adrien.todeschini@gmail.com>
2014-12-09 07:23:59 -0500

    keep only high level plots. added slightly modified knitr functions: merge_low_plot, digest_plot, is_low_change

--- R/replay-html.r
+++ R/replay-html.r
@@ -28,6 +28,9 @@ replay_html.list <- function(x, ...) {
     structure(list(src = src), class = "source")
   })
 
+  # keep only high level plots
+  parts <- merge_low_plot(parts)
+
   pieces <- character(length(parts))
   for (i in seq_along(parts)) {
     pieces[i] <- replay_html(parts[[i]], obj_id = i, ...)
@@ -92,4 +95,36 @@ replay_html.recordedplot <- function(x, pkg, name_prefix, obj_id, ...) {
   str_c("<p><img src='", escape_html(name), "' alt='' width='540' height='400' /></p>")
 }
 
+# Knitr functions ------------------------------------------------------------
+# The functions below come from package knitr (Yihui Xie) in file plot.R
+
+# get MD5 digests of recorded plots so that merge_low_plot works
+digest_plot = function(x, level = 1) {
+  if (!is.list(x) || level >= 3) return(digest::digest(x))
+  lapply(x, digest_plot, level = level + 1)
+}
+
+# merge low-level plotting changes
+merge_low_plot = function(x, idx = sapply(x, evaluate::is.recordedplot)) {
+  idx = which(idx); n = length(idx); m = NULL # store indices that will be removed
+  if (n <= 1) return(x)
+
+  # digest of recorded plots
+  rp_dg <- lapply(x[idx], digest_plot)
+
+  i1 = idx[1]; i2 = idx[2]  # compare plots sequentially
+  for (i in 1:(n - 1)) {
+    # remove the previous plot and move its index to the next plot
+    if (is_low_change(rp_dg[[i]], rp_dg[[i+1]])) m = c(m, i1)
+    i1 = idx[i + 1]
+    i2 = idx[i + 2]
+  }
+  if (is.null(m)) x else x[-m]
+}
+
+# compare two recorded plots
+is_low_change = function(p1, p2) {
+  p1 = p1[[1]]; p2 = p2[[1]]  # real plot info is in [[1]]
+  if ((n2 <- length(p2)) < (n1 <- length(p1))) return(FALSE)  # length must increase
+  identical(p1[1:n1], p2[1:n1])
 }
