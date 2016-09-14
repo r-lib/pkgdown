5463477ca66357d33bf85b162bbe992f408b2e6b
Gregory Jefferis <jefferis@gmail.com>
2016-09-14 22:32:30 +0200

    Add function to write R or yaml index files for all topics
    * the idea is that you will edit and resave these as appropriate.

--- R/index.R
+++ R/index.R
@@ -29,6 +29,57 @@ load_index_yaml <- function(pkg) {
          function (n) sd_section(n, index_list[[n]]$desc, index_list[[n]]$topics))
 }
 
+#' Write yaml index file for all help topics
+#'
+#' This function will produce a file `index-all.yaml` in `inst/staticdocs` that
+#' you can edit and resave as `index.yaml`. This will then be accepted as an
+#' alternative to the `index.R` used to generate a custom ordering of the
+#' function reference section of a static docs site.
+#'
+#' @inheritParams build_site
+#' @importFrom yaml as.yaml
+#' @export
+#' @seealso \code{\link{sd_section}}, \code{\link{write_index_r}}
+write_index_yaml <- function(pkg = '.') {
+  pkg <- as.sd_package(pkg)
+  path <- file.path.ci(pkg$sd_path, "index-all.yaml")
+  dir=dirname(path)
+  if(!file.exists(dir))
+    dir.create(dir, recursive = TRUE)
+  topics=pkg$rd_index$name
+  l=list("All topics"=list(description="", topics=topics))
+  writeLines(yaml::as.yaml(l), con = path)
+}
+
+#' Write index.R file for all help topics
+#'
+#' This function will produce a file `index-all.R` in `inst/staticdocs` that you
+#' can edit and resave as `index.R`. This will generate a custom ordering of the
+#' function reference section of a static docs site.
+#'
+#' @inheritParams build_site
+#' @importFrom yaml as.yaml
+#' @export
+#' @seealso \code{\link{sd_section}}, \code{\link{write_index_yaml}}
+write_index_r <- function(pkg = '.') {
+  pkg <- as.sd_package(pkg)
+  path <- file.path.ci(pkg$sd_path, "index-all.r")
+  dir=dirname(path)
+  if(!file.exists(dir))
+    dir.create(dir, recursive = TRUE)
+  topics=pkg$rd_index$name
+  # make R code representation of section
+  ss=substitute(sd_section("All topics", description = "", elements = topics))
+  dss=deparse(ss)
+  # some formatting
+  dss=paste(dss, collapse = "")
+  dss=gsub('",','",\n', dss, fixed = TRUE)
+  dss=gsub('c(','c(\n ', dss, fixed = TRUE)
+  dss=gsub('\\n[ ]+','\n    ',dss)
+  dss=gsub('\\)\\)$','\n))\n',dss)
+  cat(dss, file = path)
+}
+
 #' Define a section for the index page
 #'
 #' @param name Name of the section. Used as title.
