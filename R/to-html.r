49112701ef15e5464bfbfbbd1794a7c56b538727
Randy Lai <randy.cs.lai@gmail.com>
2016-09-21 17:02:32 -0400

    fix bug related to \eqn and \deqn (#102)
    * fix bug related to \eqn and \deqn
    \eqn and \deqn have two formats: \eqn{latex}{ascii} and \eqn{latexascii}
    they are correctly handled now
    
    * allow extra arguments to be passed to rd2html
    
    * add rd2html tests for deqn and eqn commands

--- R/to-html.r
+++ R/to-html.r
@@ -232,10 +232,11 @@ to_html.item <- function(x, ...) {
 #' @export
 to_html.eqn <- function(x, pkg, ...) {
   stopifnot(length(x) <= 2)
-  ascii_rep <- x[[length(x)]]
   if (pkg$mathjax){
-    str_c("$", to_html.TEXT(ascii_rep, ...), "$")
+    latex_rep <- x[[1]]
+    str_c("$", to_html.TEXT(latex_rep, ...), "$")
   }else{
+    ascii_rep <- x[[length(x)]]
     str_c("<code class = 'eq'>", to_html.TEXT(ascii_rep, ...), "</code>")
   }
 }
@@ -244,9 +245,11 @@ to_html.eqn <- function(x, pkg, ...) {
 to_html.deqn <- function(x, pkg, ...) {
   stopifnot(length(x) <= 2)
   if (pkg$mathjax){
-    str_c("$$", to_html.TEXT(x[[length(x)-1]], ...), "$$")
+    latex_rep <- x[[1]]
+    str_c("$$", to_html.TEXT(latex_rep, ...), "$$")
   }else{
-    str_c("<pre class = 'eq'>", to_html.TEXT(x[[length(x)]], ...), "</pre>")
+    ascii_rep <- x[[length(x)]]
+    str_c("<pre class = 'eq'>", to_html.TEXT(ascii_rep, ...), "</pre>")
   }
 }
 
