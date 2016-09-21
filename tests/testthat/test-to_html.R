7b44bced57f4501b3094b6821fcbf4897a72177c
Randy Lai <randy.cs.lai@gmail.com>
2016-09-21 12:18:43 -0400

    add rd2html tests for deqn and eqn commands

--- tests/testthat/test-to_html.R
+++ tests/testthat/test-to_html.R
@@ -18,3 +18,26 @@ test_that("S3 methods gets comment", {
   expect_equal(out[1], "# S3 method for class")
   expect_equal(out[2], "fun(x, y)")
 })
+
+
+test_that("eqn", {
+  out <- rd2html(" \\eqn{\\alpha}{alpha}", TRUE, pkg=list(mathjax = TRUE))
+  expect_equal(out, "$\\alpha$")
+  out <- rd2html(" \\eqn{\\alpha}{alpha}", TRUE, pkg=list(mathjax = FALSE))
+  expect_equal(out, "<code class = 'eq'>alpha</code>")
+  out <- rd2html(" \\eqn{x}", TRUE, pkg=list(mathjax = TRUE))
+  expect_equal(out, "$x$")
+  out <- rd2html(" \\eqn{x}", TRUE, pkg=list(mathjax = FALSE))
+  expect_equal(out, "<code class = 'eq'>x</code>")
+})
+
+test_that("deqn", {
+  out <- rd2html(" \\deqn{\\alpha}{alpha}", TRUE, pkg=list(mathjax = TRUE))
+  expect_equal(out, "$$\\alpha$$")
+  out <- rd2html(" \\deqn{\\alpha}{alpha}", TRUE, pkg=list(mathjax = FALSE))
+  expect_equal(out, "<pre class = 'eq'>alpha</pre>")
+  out <- rd2html(" \\deqn{x}", TRUE, pkg=list(mathjax = TRUE))
+  expect_equal(out, "$$x$$")
+  out <- rd2html(" \\deqn{x}", TRUE, pkg=list(mathjax = FALSE))
+  expect_equal(out, "<pre class = 'eq'>x</pre>")
+})
