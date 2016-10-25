7194a3fd577217e3dc3750bb78b7c9f9f80ac228
Gregory Jefferis <jefferis@gmail.com>
2016-10-25 16:37:44 +0100

    add test for remote links

--- tests/testthat/test-autolink.R
+++ tests/testthat/test-autolink.R
@@ -15,3 +15,10 @@ test_that("can find alias from help usage", {
   expect_equal(find_alias(quote(?"a-b-c")), "a-b-c")
   expect_equal(find_alias(quote(package?abc)), "abc-package")
 })
+
+context("remote links")
+
+test_that("can construct external link for package/topic", {
+  expect_equal(link_remote(label="library", topic="library", package="base"),
+               "<a href='http://www.rdocumentation.org/packages/base/topics/library'>library</a>")
+})
