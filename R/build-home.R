6e8de909ea0773150710853a058ab878374e95cb
hadley <h.wickham@gmail.com>
2016-10-26 14:10:05 -0500

    Correct fallback if no readme etc
    Fixes #184

--- R/build-home.R
+++ R/build-home.R
@@ -53,7 +53,7 @@ build_home <- function(pkg = ".", path = "docs", depth = 0L) {
     )
   } else {
     if (is.null(path)) {
-      data$index <- pkg$description
+      data$index <- pkg$desc$get("Description")[[1]]
     } else {
       data$index <- markdown(path = data$path, depth = 0L, index = pkg$topics)
     }
