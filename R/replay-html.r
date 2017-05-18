616fb5959a9498be350bbf8adf43e2b2e367776b
Forest Fang <sen.fang@airbnb.com>
2017-05-18 15:51:17 -0700

    Use exported API to freeze widget id
    This might be benefitial in case there are nested widgets in the future.

--- R/replay-html.r
+++ R/replay-html.r
@@ -117,10 +117,8 @@ replay_html.recordedplot <- function(x, name_prefix, obj_id, ...) {
 replay_html.htmlwidget <- function(x, name_prefix, obj_id, ...) {
   path <- paste0(name_prefix, obj_id, ".html")
 
-  # set widget id so it doesn't change from each rendering
-  if (is.null(x$elementId)) {
-    x$elementId <- paste0("htmlwidget-", name_prefix, obj_id)
-  }
+  # set widget id seed so it doesn't change from each rendering
+  htmlwidgets::setWidgetIdSeed(obj_id)
   htmlwidgets::saveWidget(x, path)
 
   paste0("<iframe src='", escape_html(path), "' width='100%' height='400' ></iframe>")
