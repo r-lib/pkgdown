159cbe79b4e1c1aaa02c82f59000e3da3bfcff63
Jay Hesselberth <jay.hesselberth@gmail.com>
2017-06-12 08:49:24 -0600

    Parse inst/CITATION and link to from home page. (#322)
    * Parse inst/CITATION and link to from home page.
    - closes #288
    
    * more specific link
    
    * fix typo.
    
    * qualify function
    
    * update citations
    - dropped inst/CITATION from pkgdown
    - citation link gets own section in sidebar
    - citation added at top of authors page
    
    * format citations
    - add bibtex in <pre> block
    - accommodate multiple citations

---
+++ R/build-home-citation.R
@@ -0,0 +1,47 @@
+
+has_citation <- function(path = ".") {
+  file.exists(file.path(path, 'inst/CITATION'))
+}
+
+read_citation <- function(path = ".") {
+  if (!has_citation(path)) {
+    return(character())
+  }
+  path <- file.path(path, 'inst/CITATION')
+  utils::readCitationFile(path)
+}
+
+data_home_sidebar_citation <- function(pkg = ".") {
+  pkg <- as_pkgdown(pkg)
+
+  if (!has_citation(pkg$path)) {
+    return(character())
+  }
+
+  name <- pkg$desc$get("Package")[[1]]
+  citation <- paste0("<a href='authors.html'>Citing ", name, "</li>")
+
+  list_with_heading(citation, "Citation")
+}
+
+data_citations <- function(pkg = ".") {
+  pkg <- as_pkgdown(pkg)
+  cit <- read_citation(pkg$path)
+
+  list(
+    html = format(cit, style = "html"),
+    bibtex = format(cit, style = "bibtex")
+  ) %>% purrr::transpose()
+}
+
+build_citation_authors <- function(pkg = ".", path = "docs", depth = 0L) {
+  pkg <- as_pkgdown(pkg)
+
+  data <- list(
+    pagetitle = "Citation and Authors",
+    citations = data_citations(pkg),
+    authors = data_authors(pkg)$all
+  )
+
+  render_page(pkg, "citation-authors", data, file.path(path, "authors.html"), depth = depth)
+}
