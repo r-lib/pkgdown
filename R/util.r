8583f9fd727abe4434f1eeb20e9a854bc99a974e
hadley <h.wickham@gmail.com>
2017-06-13 14:45:15 -0500

    Start refactoring highlighting and linking code
    By moving to own files

--- R/util.r
+++ R/util.r
@@ -60,55 +60,6 @@ markdown <- function(path = NULL, ..., depth = 0L, index = NULL) {
   paste(lines, collapse = "\n")
 }
 
-tweak_anchors <- function(html, only_contents = TRUE) {
-  if (only_contents) {
-    sections <- xml2::xml_find_all(html, ".//div[@class='contents']//div[@id]")
-  } else {
-    sections <- xml2::xml_find_all(html, "//div[@id]")
-  }
-
-  if (length(sections) == 0)
-    return()
-
-  # Update anchors: dot in the anchor breaks scrollspy
-  anchor <- sections %>%
-    xml2::xml_attr("id") %>%
-    gsub(".", "-", ., fixed = TRUE)
-  purrr::walk2(sections, anchor, ~ (xml2::xml_attr(.x, "id") <- .y))
-
-  # Update href of toc anchors , use "-" instead "."
-  toc_nav <- xml2::xml_find_all(html, ".//div[@id='tocnav']//a")
-  hrefs <- toc_nav %>%
-    xml2::xml_attr("href") %>%
-    gsub(".", "-", ., fixed = TRUE)
-  purrr::walk2(toc_nav, hrefs, ~ (xml2::xml_attr(.x, "href") <- .y))
-
-  headings <- xml2::xml_find_first(sections, ".//h1|h2|h3|h4|h5")
-  has_heading <- !is.na(xml2::xml_name(headings))
-
-  for (i in seq_along(headings)[has_heading]) {
-    # Insert anchor in first element of header
-    heading <- headings[[i]]
-
-    xml2::xml_attr(heading, "class") <- "hasAnchor"
-    xml2::xml_add_sibling(
-      xml2::xml_contents(heading)[[1]],
-      "a", href = paste0("#", anchor[[i]]),
-      class = "anchor",
-      .where = "before"
-    )
-  }
-  invisible()
-}
-
-tweak_tables <- function(html) {
-  # Ensure all tables have class="table"
-  table <- xml2::xml_find_all(html, ".//table")
-  xml2::xml_attr(table, "class") <- "table"
-
-  invisible()
-}
-
 set_contains <- function(haystack, needles) {
   all(needles %in% haystack)
 }
