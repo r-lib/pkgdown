9a6e137229016e1a35d6a251058b5a8548d77a56
hadley <h.wickham@gmail.com>
2017-06-14 14:53:20 -0500

    Lookup remote reference urls
    By inspecting each Url in the description, looking for pkgdown.yml

---
+++ R/remote.R
@@ -0,0 +1,31 @@
+#' @importFrom memoise memoise
+NULL
+
+remote_package_url <- memoise(function(package) {
+  path <- find.package(package, quiet = TRUE)
+  if (length(path) == 0) {
+    return(NULL)
+  }
+
+  desc <- read_desc(path)
+  urls <- desc$get_urls()
+
+  for (url in urls) {
+    url <- paste0(url, "/pkgdown.yml")
+
+    yaml <- tryCatch(fetch_yaml(url), error = function(e) NULL)
+    if (!is.null(yaml)) {
+      return(yaml$reference_url)
+    }
+  }
+
+  NULL
+})
+
+fetch_yaml <- function(url) {
+  resp <- httr::GET(url, httr::timeout(3))
+  httr::stop_for_status(resp)
+
+  text <- httr::content(resp, as = "text", encoding = "UTF-8")
+  yaml::yaml.load(text)
+}
