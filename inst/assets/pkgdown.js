f200e2843254c6ff177ada38cb1e872a25190b2c
Hadley Wickham <h.wickham@gmail.com>
2017-06-20 16:41:36 -0500

    Use js to indicate activate nav category (#361)
    Fixes #326

--- inst/assets/pkgdown.js
+++ inst/assets/pkgdown.js
@@ -5,4 +5,33 @@ $(function() {
     offset: 60
   });
 
+  var cur_path = paths(location.pathname);
+  $("#navbar ul li a").each(function(index, value) {
+    var path = paths(value.pathname);
+    if (is_prefix(cur_path, path)) {
+      $(value.parentElement).addClass("active");
+    }
+  });
 });
+
+function paths(pathname) {
+  var pieces = pathname.split("/");
+  pieces.shift(); // always starts with /
+
+  var end = pieces[pieces.length - 1];
+  if (end === "index.html" || end === "")
+    pieces.pop();
+  return(pieces);
+}
+
+function is_prefix(needle, haystack) {
+  if (needle.length > haystack.lengh)
+    return(false);
+
+  for (var i = 0; i < haystack.length; i++) {
+    if (needle[i] != haystack[i])
+      return(false);
+  }
+
+  return(true);
+}
