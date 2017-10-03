42fb512855591b77454c765b4c407938fba38158
Romain Lesur <romain.lesur@justice.gouv.fr>
2017-10-03 17:12:20 +0200

    Add copy to clipboard buttons to examples in reference pages

--- inst/assets/pkgdown.js
+++ inst/assets/pkgdown.js
@@ -43,3 +43,43 @@ function is_prefix(needle, haystack) {
 
   return(true);
 }
+
+/* Clipboard --------------------------*/
+
+function changeTooltipMessage(element, msg) {
+  var tooltipOriginalTitle=element.getAttribute('data-original-title');
+  element.setAttribute('data-original-title', msg);
+  $(element).tooltip('show');
+  element.setAttribute('data-original-title', tooltipOriginalTitle);
+}
+
+if(Clipboard.isSupported()) {
+  $(document).ready(function() {
+    var copyButton = "<button type='button' class='btn btn-primary btn-copy-ex' type = 'submit' title='Copy to clipboard' aria-hidden='true' data-toggle='tooltip' data-placement='left auto' data-trigger='hover' data-clipboard-copy><i class='fa fa-copy' aria-hidden='true'></i></button>";
+
+    $(".examples").addClass("hasCopyButton");
+
+    // Insert copy buttons:
+    $(copyButton).prependTo(".hasCopyButton");
+
+    // Initialize tooltips:
+    $('.btn-copy-ex').tooltip({container: 'body'});
+
+    // Initialize clipboard:
+    var clipboardBtnCopies = new Clipboard('[data-clipboard-copy]', {
+      text: function(trigger) {
+        return trigger.parentNode.textContent;
+      }
+    });
+
+    clipboardBtnCopies.on('success', function(e) {
+      changeTooltipMessage(e.trigger, 'Copied!');
+      e.clearSelection();
+    });
+
+    clipboardBtnCopies.on('error', function() {
+      changeTooltipMessage(e.trigger,'Press Ctrl+C or Command+C to copy');
+    });
+  });
+}
+
