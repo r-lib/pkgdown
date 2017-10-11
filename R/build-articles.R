ce1b13b92d14c34f6182a415f54cddedcdc95f57
Jim Hester <james.f.hester@gmail.com>
2017-10-11 16:29:02 -0400

    Colourise R output using ANSI escapes
    If R output includes ANSI escape sequences, convert them to HTML
    equivalents.

--- R/build-articles.R
+++ R/build-articles.R
@@ -115,14 +115,19 @@ render_rmd <- function(pkg,
   on.exit(unlink(format$path), add = TRUE)
 
   path <- callr::r_safe(
-    function(...) rmarkdown::render(...),
+    function(..., crayon.enabled, crayon.colors) {
+      options("crayon.enabled" = crayon.enabled, "crayon.colors" = crayon.colors)
+      rmarkdown::render(...)
+    },
     args = list(
       input,
       output_format = format$format,
       output_file = basename(output_file),
       quiet = quiet,
       encoding = encoding,
-      envir = globalenv()
+      envir = globalenv(),
+      crayon.enabled = getOption("crayon.enabled", crayon::has_color()),
+      crayon.colors = getOption("crayon.colors", crayon::num_colors())
     ),
     show = !quiet
   )
@@ -139,16 +144,30 @@ build_rmarkdown_format <- function(pkg = ".",
     render_page(pkg, "vignette", data, path, depth = depth)
   )
 
-  list(
-    path = path,
-    format = rmarkdown::html_document(
+  format <- rmarkdown::html_document(
       toc = toc,
       toc_depth = 2,
       self_contained = FALSE,
       theme = NULL,
-      template = path
-    )
-  )
+      template = path)
+
+  format$knitr$knit_hooks <-
+    list(
+      output = colourise_chunk("output"),
+      message = colourise_chunk("message"),
+      warning = colourise_chunk("warning"),
+      error = colourise_chunk("error"))
+
+  list(path = path, format = format)
+}
+
+colourise_chunk <- function(type) {
+  function(x, options) {
+  sprintf('<div class = "%s"><pre class="knitr %s">%s</pre></div>\n',
+    type,
+    tolower(options$engine),
+    ansistrings::ansi_to_html(x, fullpage = FALSE))
+  }
 }
 
 update_rmarkdown_html <- function(path, strip_header = FALSE, depth = 1L) {
