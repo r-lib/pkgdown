e1a3c2cf8501b4cef34601559dee3ade011eb06a
Jay Hesselberth <jay.hesselberth@gmail.com>
2018-01-22 07:52:30 -0700

    add failing test for citation(auto = meta) - part of #493

--- tests/testthat/test-citation_authors.R
+++ tests/testthat/test-citation_authors.R
@@ -1,7 +1,13 @@
 context("build citation/authors")
 
 test_that("citations are read with explicit encoding (#416)", {
-  path <- test_path('data-citation')
+  path <- test_path('data-citation/encoding')
+  cit <- read_citation(path, encoding = 'UTF-8')
+  expect_is(cit, 'citation')
+})
+
+test_that("citation(auto = meta) does not error (#493)", {
+  path <- test_path('data-citation/auto-meta')
   cit <- read_citation(path, encoding = 'UTF-8')
   expect_is(cit, 'citation')
 })
