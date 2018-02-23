b9007890c558c5102ee5d76473b0451639be72e8
Sebastian Meyer <seb.meyer@fau.de>
2018-02-23 10:44:59 +0100

    has_vignettes() seems to be obsolete now

--- R/build-articles.R
+++ R/build-articles.R
@@ -257,8 +257,3 @@ default_articles_index <- function(pkg = ".") {
   ))
 
 }
-
-has_vignettes <- function(path = ".") {
-  vign_path <- file.path(path, "vignettes")
-  file.exists(vign_path) && length(list.files(vign_path))
-}
