cda1ea09a9cce3e8019eb6a3ab72ba59632f1739
Jay Hesselberth <jay.hesselberth@gmail.com>
2018-04-09 09:17:07 -0600

    test that search files are valid

---
+++ tests/testthat/test-build-search-docs.R
@@ -0,0 +1,15 @@
+context("test-build-search-docs.R")
+
+test_that("docsearch.json and sitemap.xml are valid", {
+  search <- test_path("search-site")
+  on.exit(clean_site(search))
+
+  expect_output(init_site(search))
+  json <- path(search, "docs", "docsearch.json")
+  expect_true(jsonlite::validate(readLines(json)))
+
+  xml <- path(search, "docs", "sitemap.xml")
+  schema <- xml2::read_xml(path(search, "sitemaps-schema-0.9.xsd"))
+  expect_true(xml2::xml_validate(xml2::read_xml(xml), schema))
+})
+
