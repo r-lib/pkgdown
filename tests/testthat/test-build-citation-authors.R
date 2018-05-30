1935bfea1d48f155bd1ecfa5c039f883b28a48f4
Jay Hesselberth <jay.hesselberth@gmail.com>
2018-05-30 08:45:47 -0600

    read CITATION with latin1 encoding and citation(auto = meta)
    ports test from #689 and code from #679
    
    need to confirm whether this addresses #672, which seems more likely to be a windows bug.
    
    closes #689, closes #679

--- tests/testthat/test-build-citation-authors.R
+++ tests/testthat/test-build-citation-authors.R
@@ -4,8 +4,16 @@ context("test-build-citation-authors.R")
 # so 'site-citation' is build-ignored, and so the tests must be skipped
 # during R CMD check
 
-test_that("CITATION with non-ASCII author and `citation(auto = meta) can be read` (#416, #493)", {
-  path <- test_path("assets/site-citation")
+test_that("UTF-8 encoding and `citation(auto = meta) can be read` (#416, #493)", {
+  path <- test_path("assets/site-citation/encoding-UTF-8")
+  skip_if_not(dir_exists(path)[[1]])
+
+  cit <- read_citation(path)
+  expect_is(cit, "citation")
+})
+
+test_that("latin1 encoding and `citation(auto = meta) can be read` (#689)", {
+  path <- test_path("assets/site-citation/encoding-latin1")
   skip_if_not(dir_exists(path)[[1]])
 
   cit <- read_citation(path)
