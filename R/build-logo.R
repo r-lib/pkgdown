73d731163423510042d1214db492b2a03ddb28ed
Hugo <hugo.gruson@protonmail.com>
2018-11-09 14:56:13 +0100

    Generate favicon using realfavicongenerator API

--- R/build-logo.R
+++ R/build-logo.R
@@ -7,19 +7,36 @@ build_logo <- function(pkg = ".") {
 
   file_copy_to(pkg, logo_path, from_dir = path_dir(logo_path))
 
-  if (!requireNamespace("magick", quietly = TRUE)) {
-    message("magick not available, using default pkgdown favicon.ico")
-
-    if (!file_exists(path(pkg$dst_path, "favicon.ico"))) {
-      file_copy(path_pkgdown("assets", "favicon.ico"), pkg$dst_path)
-    }
-    return()
-  }
-
   cat_line("Creating ", dst_path("favicon.ico"))
-  magick::image_read(logo_path) %>%
-	  magick::image_scale("32x32") %>%
-	  magick::image_write(path(pkg$dst_path, "favicon.ico"), format = "png")
+
+  logo_string <- readBin(logo_path, what = "raw", n = 99999) %>%
+    openssl::base64_encode()
+
+  template <- find_template("config", "favicon", ext = ".json")
+
+  data <- list(
+    url = pkg$meta$url,
+    logo_string = logo_string
+  )
+
+  json_favicon <- render_template(template, data)
+
+  request <- httr::POST("https://realfavicongenerator.net/api/favicon",
+                        body = json_favicon, encode = "json")
+
+  api_answer <- httr::content(request)
+
+  if (api_answer$favicon_generation_result$result$status == "success") {
+
+    result <- httr::GET(api_answer$favicon_generation_result$favicon$package_url)
+
+    writeBin(httr::content(result), path(pkg$dst_path, "favicon_set.zip"))
+
+    unzip(path(pkg$dst_path, "favicon_set.zip"), exdir = pkg$dst_path)
+
+    unlink(path(pkg$dst_path, "favicon_set.zip"))
+
+  }
 }
 
 
