ac2bac0358bb73f10ffeb56e7f0177654d19941d
Hugo Gruson <hugo.gruson@protonmail.com>
2018-11-17 16:00:57 +0100

    Default to navbar-light navbar-default does not exist in bootstrap 4

--- R/navbar.R
+++ R/navbar.R
@@ -19,7 +19,7 @@ data_navbar <- function(pkg = ".", depth = 0L) {
   right <- navbar$right %||% components[right_comp]
 
   list(
-    type = navbar$type %||% "default",
+    type = navbar$type %||% "light",
     left = render_navbar_links(left, depth = depth),
     right = render_navbar_links(right, depth = depth)
   )
