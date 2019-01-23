8269516ef65689b997c407b30e0779badb78f900
Andrie de Vries <apdevries@gmail.com>
2019-01-23 19:19:17 +0000

    Catch and deal with case when typeof() == "character" #965

--- R/usage.R
+++ R/usage.R
@@ -78,8 +78,10 @@ usage_type <- function(x) {
     }
 
     out
+  } else if (is.character(x)) {
+    list(type = "data", name = x)
   } else {
-    stop("Unknown type: ", typeof(x), call. = FALSE)
+    stop("Unknown type: ", typeof(x), " (in ", as.character(x), ")",  call. = FALSE)
   }
 }
 
