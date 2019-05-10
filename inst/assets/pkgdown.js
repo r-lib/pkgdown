f6e70ca477c4514019fafe947a597a37f3430a7d
Hugo Gruson <hugo.gruson@protonmail.com>
2019-05-10 15:56:03 +0200

    Implement sticky navbar in css See https://developer.mozilla.org/en-US/docs/Mozilla/Performance/Scroll-linked_effects

--- inst/assets/pkgdown.js
+++ inst/assets/pkgdown.js
@@ -2,15 +2,6 @@
 (function($) {
   $(function() {
 
-    $("#sidebar")
-      .stick_in_parent({offset_top: 40})
-      .on('sticky_kit:bottom', function(e) {
-        $(this).parent().css('position', 'static');
-      })
-      .on('sticky_kit:unbottom', function(e) {
-        $(this).parent().css('position', 'relative');
-      });
-
     $('body').scrollspy({
       target: '#sidebar',
       offset: 60
