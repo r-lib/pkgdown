c63ddd2ffc1158c081131f019decc9157c266cb7
Jay Hesselberth <jay.hesselberth@gmail.com>
2019-06-01 06:54:32 -0600

    Skip test if offline

--- tests/testthat/test-build-favicons.R
+++ tests/testthat/test-build-favicons.R
@@ -1,6 +1,9 @@
 context("test-build-favicons.R")
 
 test_that("bad logos generate a warning", {
+  skip_on_cran() # requires internet connection
+  skip_if_offline()
+
   pkg <- as_pkgdown(test_path("assets/site-bad-logo"))
   on.exit(dir_delete(path(pkg$src_path, "pkgdown")))
 
