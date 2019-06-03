4673d7314e04f36d34d75428d60ec7cb01f54857
Jay Hesselberth <jay.hesselberth@gmail.com>
2019-06-03 08:20:04 -0600

    Use dst_path

--- R/clean.R
+++ R/clean.R
@@ -7,7 +7,7 @@
 clean_site <- function(pkg = ".") {
   pkg <- as_pkgdown(pkg)
 
-  if (!dir_exists(path(pkg$src_path, "docs"))) return(invisible())
+  if (!dir_exists(pkg$dst_path)) return(invisible())
 
   top_level <- dir_ls(pkg$dst_path)
   top_level <- top_level[!path_file(top_level) %in% c("CNAME", "dev")]
