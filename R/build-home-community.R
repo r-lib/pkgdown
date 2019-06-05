b45c74c77c6bc935ae3570dd5773f97c71392e74
Maëlle Salmon <maelle.salmon@yahoo.se>
2019-06-05 17:37:33 +0200

    review comments

--- R/build-home-community.R
+++ R/build-home-community.R
@@ -1,23 +1,29 @@
 has_contributing <- function(path = ".") {
-  file_exists(path(path, 'CONTRIBUTING.html'))
+  file_exists(path(path, 'CONTRIBUTING.md')) |
+    file_exists(path(path, '.github', 'CONTRIBUTING.md'))
 }
 
 has_coc <- function(path = ".") {
-  file_exists(path(path, 'CODE_OF_CONDUCT.html'))
+  file_exists(path(path, 'CODE_OF_CONDUCT.md')) |
+    file_exists(path(path, '.github', 'CODE_OF_CONDUCT.md'))
 }
 
 data_home_sidebar_community <- function(pkg){
 
   links <- NULL
 
-  if (has_contributing(pkg$dst_path)) {
-    links <- c(links,
-               '<a href="CONTRIBUTING.html">Contributing guide</a>')
+  if (has_contributing(pkg$src_path)) {
+    links <- c(
+      links,
+      '<a href="CONTRIBUTING.html">Contributing guide</a>'
+    )
   }
 
-  if (has_coc(pkg$dst_path)) {
-    links <- c(links,
-               '<a href="CODE_OF_CONDUCT.html">Code of conduct</a>')
+  if (has_coc(pkg$src_path)) {
+    links <- c(
+      links,
+      '<a href="CODE_OF_CONDUCT.html">Code of conduct</a>'
+    )
   }
 
 
@@ -28,5 +34,3 @@ data_home_sidebar_community <- function(pkg){
   sidebar_section("Community", links)
 
 }
-
-
