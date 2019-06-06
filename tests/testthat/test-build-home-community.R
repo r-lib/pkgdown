523615eedfda90ee4997bf421b89393410a8eed2
Maëlle Salmon <maelle.salmon@yahoo.se>
2019-06-06 07:34:39 +0200

    copy comment

--- tests/testthat/test-build-home-community.R
+++ tests/testthat/test-build-home-community.R
@@ -1,6 +1,8 @@
 context("test-build-home-community.R")
 
 test_that("has_coc works", {
+  # .github in this test is build-ignored to prevent a NOTE about an unexpected
+  # hidden directory. Skip on CMD CHECK if the .github directory is not present.s
   skip_if_not(dir_exists(path(test_path("assets/site-dot-github"),
                               ".github"))[[1]])
   expect_true(has_coc(test_path("assets/site-dot-github")))
@@ -14,6 +16,9 @@ test_that("has_contributing works", {
 test_that("community section is added if COC present", {
 
   pkg <- test_path("assets/site-dot-github")
+
+  # .github in this test is build-ignored to prevent a NOTE about an unexpected
+  # hidden directory. Skip on CMD CHECK if the .github directory is not present.
   skip_if_not(dir_exists(path(pkg, ".github"))[[1]])
 
   comm <- data_home_sidebar_community(pkg)
