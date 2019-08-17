159791de3e59fe121cf0318d90784f65e14871a6
Jay Hesselberth <jay.hesselberth@gmail.com>
2019-08-17 10:14:46 -0600

    JS and CSS tweaks

--- inst/assets/pkgdown.js
+++ inst/assets/pkgdown.js
@@ -4,11 +4,6 @@
 
     $('nav.navbar').headroom();
 
-    $('body').css('padding-top', $('.navbar').height() + 10);
-    $(window).resize(function(){
-      $('body').css('padding-top', $('.navbar').height() + 10);
-    });
-
     $('body').scrollspy({
       target: '#sidebar',
       offset: 60
