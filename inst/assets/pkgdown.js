23affb226428307b8ca89ef3b33b5ed66de28b2a
Jay Hesselberth <jay.hesselberth@gmail.com>
2019-08-20 06:47:05 -0600

    Fix clipboard

--- inst/assets/pkgdown.js
+++ inst/assets/pkgdown.js
@@ -78,7 +78,7 @@
 
   if(ClipboardJS.isSupported()) {
     $(document).ready(function() {
-      var copyButton = "<button type='button' class='btn btn-primary btn-copy-ex' type = 'submit' title='Copy to clipboard' aria-label='Copy to clipboard' data-toggle='tooltip' data-placement='left auto' data-trigger='hover' data-clipboard-copy><i class='fa fa-copy'></i></button>";
+      var copyButton = "<button type='button' class='btn btn-primary btn-copy-ex' title='Copy to clipboard' aria-label='Copy to clipboard' data-toggle='tooltip' data-placement='left' data-trigger='hover' data-clipboard-copy><i class='fa fa-copy'></i></button>";
 
       $(".examples, div.sourceCode").addClass("hasCopyButton");
 
@@ -89,20 +89,21 @@
       $('.btn-copy-ex').tooltip({container: 'body'});
 
       // Initialize clipboard:
-      var clipboardBtnCopies = new ClipboardJS('[data-clipboard-copy]', {
+      var clipboard = new ClipboardJS('[data-clipboard-copy]', {
         text: function(trigger) {
           return trigger.parentNode.textContent;
         }
       });
 
-      clipboardBtnCopies.on('success', function(e) {
+      clipboard.on('success', function(e) {
         changeTooltipMessage(e.trigger, 'Copied!');
         e.clearSelection();
       });
 
-      clipboardBtnCopies.on('error', function() {
+      clipboard.on('error', function() {
         changeTooltipMessage(e.trigger,'Press Ctrl+C or Command+C to copy');
       });
+
     });
   }
 })(window.jQuery || window.$)
