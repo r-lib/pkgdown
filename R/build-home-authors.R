9ef72f05430a158b1e7be0d6b551141d3e4e4394
Hugo Gruson <hugo.gruson@protonmail.com>
2019-09-23 15:55:33 +0200

    Add aria semantic attributes for accessibility

--- R/build-home-authors.R
+++ R/build-home-authors.R
@@ -152,8 +152,8 @@ orcid_link <- function(orcid) {
   if (is.null(orcid)) return(NULL)
 
   paste0(
-    "<a href='https://orcid.org/", orcid, "' target='orcid.widget'>",
-    "<span class='fab fa-orcid orcid'></span></a>"
+    "<a href='https://orcid.org/", orcid, "' target='orcid.widget' aria-label='ORCID'>",
+    "<span class='fab fa-orcid orcid' aria-hidden='true'></span></a>"
   )
 }
 
