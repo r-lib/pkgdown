df53c9ec68defeeb3a947d00195bb938dfe62c8f
Kevin Ushey <kevinushey@gmail.com>
2019-12-02 10:07:17 -0800

    use helper

--- tests/testthat/test-build-version.R
+++ tests/testthat/test-build-version.R
@@ -4,6 +4,6 @@ test_that("formatting in DESCRIPTION version is preserved", {
   pkg <- as_pkgdown(test_path("assets/version-formatting"))
   expect_equal(pkg$version, "1.0.0-9000")
   build_home_index(pkg, quiet = TRUE)
-  index <- read_lines(file.path(pkg$dst_path, "index.html"))
+  index <- read_lines(path(pkg$dst_path, "index.html"))
   expect_true(any(grepl("1.0.0-9000", index, fixed = TRUE)))
 })
