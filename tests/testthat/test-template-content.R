fd5ad5e238fd4d67955e9085a4b45e3726c7ded8
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2020-01-31 14:31:56 -0800

    Refactored original open graph tests
    The reference page tests are still slow, though

--- tests/testthat/test-template-content.R
+++ tests/testthat/test-template-content.R
@@ -6,39 +6,60 @@ n_matches <- function(page, pattern) {
 
 # Open Graph ------------------------------------------
 
-test_that("og tags are populated on home, reference, and articles", {
+describe("og tags are populated on home, reference, and articles", {
   skip_if_no_pandoc()
 
   pkg <- as_pkgdown(test_path("assets/open-graph"))
-  setup(expect_output(build_site(pkg, new_process = FALSE)))
-  on.exit(clean_site(pkg))
-  on.exit(dir_delete(path(pkg$src_path, "pkgdown")))
+  pkg_tmp_dir <- dir_create(file_temp("pkgdown-open-graph-standard"))
+  pkg$dst_path <- pkg_tmp_dir
+  on.exit(unlink(pkg_tmp_dir))
 
-  index_html <- read_lines(path(pkg$dst_path, "index.html"))
-  desc <- '<meta property="og:description" content="A longer statement about the package.">'
-  expect_true(desc %in% index_html)
-  img <- '<meta property="og:image" content="http://example.com/pkg/logo.png">'
-  expect_true(img %in% index_html)
+  it("populates og tags on home", {
+    index_html_path <- path(pkg_tmp_dir, "index.html")
+    expect_output(render_page(pkg, "home", data_home(pkg), path = index_html_path))
 
-  pork_html <- read_lines(path(pkg$dst_path, "reference", "f.html"))
-  desc <- '<meta property="og:description" content="Title" />'
-  expect_true(desc %in% pork_html)
-  img <- '<meta property="og:image" content="http://example.com/pkg/logo.png" />'
-  expect_true(img %in% pork_html)
+    index_html <- read_lines(index_html_path)
+    desc <- '<meta property="og:description" content="A longer statement about the package."'
+    img <- '<meta property="og:image" content="http://example.com/pkg/logo.png"'
+    expect_equal(n_matches(index_html, desc), 1)
+    expect_equal(n_matches(index_html, img), 1)
+  })
 
-  vignette_html <- read_lines(path(pkg$dst_path, "articles", "open-graph.html"))
-  desc <- '<meta property="og:description" content="The Open Graph protocol is a standard for web page metadata.">'
-  expect_true(desc %in% vignette_html)
-  img <- '<meta property="og:image" content="http://example.com/pkg/logo.png">'
-  expect_true(img %in% vignette_html)
+  it("populates og tags on reference", {
+    ref_page_path <- path(pkg_tmp_dir, "reference", "f.html")
+    dir_create(pkg_tmp_dir, "reference")
+    with_dir(
+      pkg_tmp_dir,
+      expect_output(build_reference(pkg, preview = FALSE))
+    )
+    ref_html <- read_lines(ref_page_path)
+
+    desc <- '<meta property="og:description" content="Title" />'
+    img <- '<meta property="og:image" content="http://example.com/pkg/logo.png" />'
+    expect_equal(n_matches(ref_html, desc), 1)
+    expect_equal(n_matches(ref_html, img), 1)
+  })
+
+  it("populates og tags on vignettes", {
+    dir_create(pkg_tmp_dir, "articles")
+    expect_output(build_article("open-graph", pkg = pkg, quiet = TRUE))
+    vignette_html_path <- path(pkg_tmp_dir, "articles", "open-graph.html")
+    vignette_html <- read_lines(vignette_html_path)
+
+    desc <- '<meta property="og:description" content="The Open Graph protocol is a standard for web page metadata.">'
+    img <- '<meta property="og:image" content="http://example.com/pkg/logo.png">'
+    expect_equal(n_matches(vignette_html, desc), 1)
+    expect_equal(n_matches(vignette_html, img), 1)
+  })
 })
 
 test_that("if there is no logo.png, there is no og:image tag", {
   skip_if_no_pandoc()
 
   pkg <- as_pkgdown(test_path("assets/home-readme-rmd"))
-  expect_output(build_site(pkg, new_process = FALSE))
-  on.exit(clean_site(pkg))
+  pkg_tmp_dir <- dir_create(file_temp("pkgdown-no-logo"))
+  pkg$dst_path <- pkg_tmp_dir
+  on.exit(unlink(pkg_tmp_dir))
 
   index_html_path <- path(pkg_tmp_dir, "index.html")
   expect_output(render_page(pkg, "home", data_home(pkg), path = index_html_path))
