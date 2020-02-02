f1a2b3dcbaccb675c636bf678adcc1b3d56af074
Kathleen Wendt <wendtke@colostate.edu>
2020-02-02 12:33:27 -0700

    revise doc submenu example from ijlyttle comment

--- R/build.r
+++ R/build.r
@@ -155,10 +155,7 @@
 #'   articles:
 #'     text: Articles
 #'     menu:
-#'     components:
 #'     - text: Category A
-#'      - text: Sub-topic 1
-#'      - text: Sub-topic 2
 #'     - text: Title A1
 #'       href: articles/a1.html
 #'     - text: Title A2
@@ -166,7 +163,9 @@
 #'     - text: -------
 #'     - text: "Category B"
 #'     - text: Title B1
-#'       href: articles/b1.html
+#'       menu:
+#'       - text "Sub-category B11"
+#'         href: articles/b11.html
 #'    twitter:
 #'      icon: "fab fa-twitter fa-lg"
 #'      href: http://twitter.com/hadleywickham
