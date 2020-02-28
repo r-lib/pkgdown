c26022038a51c6e1dd0add562e274c9865f74794
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2020-02-28 14:35:52 -0500

    Update test styles in test-template-content to match package style

--- tests/testthat/test-template-content.R
+++ tests/testthat/test-template-content.R
@@ -6,7 +6,7 @@ n_matches <- function(page, pattern) {
 
 # Open Graph ------------------------------------------
 
-describe("og tags are populated on home, reference, and articles", {
+test_that("opengraph tags are populated on home, reference, and articles", {
   skip_if_no_pandoc()
 
   pkg <- as_pkgdown(test_path("assets/open-graph"))
@@ -14,43 +14,40 @@ describe("og tags are populated on home, reference, and articles", {
   pkg$dst_path <- pkg_tmp_dir
   on.exit(unlink(pkg_tmp_dir))
 
-  it("populates og tags on home", {
-    index_html_path <- path(pkg_tmp_dir, "index.html")
-    expect_output(render_page(pkg, "home", data_home(pkg), path = index_html_path))
+  # opengrah tags are populated on home page
+  index_html_path <- path(pkg_tmp_dir, "index.html")
+  expect_output(render_page(pkg, "home", data_home(pkg), path = index_html_path))
 
-    index_html <- read_lines(index_html_path)
-    desc <- '<meta property="og:description" content="A longer statement about the package."'
-    img <- '<meta property="og:image" content="http://example.com/pkg/logo.png"'
-    expect_equal(n_matches(index_html, desc), 1)
-    expect_equal(n_matches(index_html, img), 1)
-  })
+  index_html <- read_lines(index_html_path)
+  desc <- '<meta property="og:description" content="A longer statement about the package."'
+  img <- '<meta property="og:image" content="http://example.com/pkg/logo.png"'
+  expect_equal(n_matches(index_html, desc), 1)
+  expect_equal(n_matches(index_html, img), 1)
 
-  it("populates og tags on reference", {
-    ref_page_path <- path(pkg_tmp_dir, "reference", "f.html")
-    dir_create(pkg_tmp_dir, "reference")
-    with_dir(
-      pkg_tmp_dir,
-      expect_output(build_reference(pkg, examples = FALSE, topics = "f"))
-    )
-    ref_html <- read_lines(ref_page_path)
+  # opengrah tags are populated in reference pages
+  ref_page_path <- path(pkg_tmp_dir, "reference", "f.html")
+  dir_create(pkg_tmp_dir, "reference")
+  with_dir(
+    pkg_tmp_dir,
+    expect_output(build_reference(pkg, examples = FALSE, topics = "f"))
+  )
+  ref_html <- read_lines(ref_page_path)
 
-    desc <- '<meta property="og:description" content="Title" />'
-    img <- '<meta property="og:image" content="http://example.com/pkg/logo.png" />'
-    expect_equal(n_matches(ref_html, desc), 1)
-    expect_equal(n_matches(ref_html, img), 1)
-  })
+  desc <- '<meta property="og:description" content="Title" />'
+  img <- '<meta property="og:image" content="http://example.com/pkg/logo.png" />'
+  expect_equal(n_matches(ref_html, desc), 1)
+  expect_equal(n_matches(ref_html, img), 1)
 
-  it("populates og tags on vignettes", {
-    dir_create(pkg_tmp_dir, "articles")
-    expect_output(build_article("open-graph", pkg = pkg, quiet = TRUE))
-    vignette_html_path <- path(pkg_tmp_dir, "articles", "open-graph.html")
-    vignette_html <- read_lines(vignette_html_path)
+  # opengraph tags are populated in vignettes
+  dir_create(pkg_tmp_dir, "articles")
+  expect_output(build_article("open-graph", pkg = pkg, quiet = TRUE))
+  vignette_html_path <- path(pkg_tmp_dir, "articles", "open-graph.html")
+  vignette_html <- read_lines(vignette_html_path)
 
-    desc <- '<meta property="og:description" content="The Open Graph protocol is a standard for web page metadata.">'
-    img <- '<meta property="og:image" content="http://example.com/pkg/logo.png">'
-    expect_equal(n_matches(vignette_html, desc), 1)
-    expect_equal(n_matches(vignette_html, img), 1)
-  })
+  desc <- '<meta property="og:description" content="The Open Graph protocol is a standard for web page metadata.">'
+  img <- '<meta property="og:image" content="http://example.com/pkg/logo.png">'
+  expect_equal(n_matches(vignette_html, desc), 1)
+  expect_equal(n_matches(vignette_html, img), 1)
 })
 
 test_that("if there is no logo.png, there is no og:image tag", {
@@ -68,7 +65,7 @@ test_that("if there is no logo.png, there is no og:image tag", {
 })
 
 
-describe("customized open graph tags", {
+test_that("customized open graph tags are populated on home, reference, and articles", {
   skip_if_no_pandoc()
   pkg <- as_pkgdown(test_path("assets/open-graph-customized"))
   pkg_tmp_dir <- dir_create(file_temp("pkgdown-open-graph"))
@@ -83,58 +80,55 @@ describe("customized open graph tags", {
   twitter_site <- '<meta name="twitter:site" content="@rstudio"'
   twitter_card <- '<meta name="twitter:card" content="summary_large_image"'
 
-  it("OpenGraph tags are populated on home (index.html)", {
-    index_html_path <- path(pkg_tmp_dir, "index.html")
-    expect_output(render_page(pkg, "home", data_home(pkg), path = index_html_path))
-    index_html <- read_lines(index_html_path)
+  # OpenGraph tags are populated on home (index.html)
+  index_html_path <- path(pkg_tmp_dir, "index.html")
+  expect_output(render_page(pkg, "home", data_home(pkg), path = index_html_path))
+  index_html <- read_lines(index_html_path)
 
-    expect_equal(n_matches(index_html, desc), 1)
-    expect_equal(n_matches(index_html, img), 1)
-    expect_equal(n_matches(index_html, img_alt), 1)
-    expect_equal(n_matches(index_html, twitter_creator), 1)
-    expect_equal(n_matches(index_html, twitter_site), 1)
-    expect_equal(n_matches(index_html, twitter_card), 1)
-  })
+  expect_equal(n_matches(index_html, desc), 1)
+  expect_equal(n_matches(index_html, img), 1)
+  expect_equal(n_matches(index_html, img_alt), 1)
+  expect_equal(n_matches(index_html, twitter_creator), 1)
+  expect_equal(n_matches(index_html, twitter_site), 1)
+  expect_equal(n_matches(index_html, twitter_card), 1)
 
-  it("OpenGraph tags are populated in reference pages", {
-    ref_page_path <- path(pkg_tmp_dir, "reference", "f.html")
-    dir_create(pkg_tmp_dir, "reference")
-    with_dir(
-      pkg_tmp_dir,
-      expect_output(build_reference(pkg, examples = FALSE, topics = "f"))
-    )
-    ref_page <- read_lines(ref_page_path)
-    desc <- '<meta property="og:description" content="Title"'
+  # OpenGraph tags are populated in reference pages
+  ref_page_path <- path(pkg_tmp_dir, "reference", "f.html")
+  dir_create(pkg_tmp_dir, "reference")
+  with_dir(
+    pkg_tmp_dir,
+    expect_output(build_reference(pkg, examples = FALSE, topics = "f"))
+  )
+  ref_page <- read_lines(ref_page_path)
+  desc <- '<meta property="og:description" content="Title"'
 
-    expect_equal(n_matches(ref_page, desc), 1)
-    expect_equal(n_matches(ref_page, img), 1)
-    expect_equal(n_matches(ref_page, img_alt), 1)
-    expect_equal(n_matches(ref_page, twitter_creator), 1)
-    expect_equal(n_matches(ref_page, twitter_site), 1)
-    expect_equal(n_matches(ref_page, twitter_card), 1)
-  })
+  expect_equal(n_matches(ref_page, desc), 1)
+  expect_equal(n_matches(ref_page, img), 1)
+  expect_equal(n_matches(ref_page, img_alt), 1)
+  expect_equal(n_matches(ref_page, twitter_creator), 1)
+  expect_equal(n_matches(ref_page, twitter_site), 1)
+  expect_equal(n_matches(ref_page, twitter_card), 1)
 
-  it("OpenGraph tags are populated in articles", {
-    dir_create(pkg_tmp_dir, "articles")
-    expect_warning(
-      expect_output(build_article("open-graph", pkg = pkg, quiet = TRUE)),
-      "Unsupported `opengraph` field:"
-    )
-    articles_page_path <- path(pkg_tmp_dir, "articles", "open-graph.html")
-    articles_page <- read_lines(articles_page_path)
+  # OpenGraph tags are populated in articles
+  dir_create(pkg_tmp_dir, "articles")
+  expect_warning(
+    expect_output(build_article("open-graph", pkg = pkg, quiet = TRUE)),
+    "Unsupported `opengraph` field:"
+  )
+  articles_page_path <- path(pkg_tmp_dir, "articles", "open-graph.html")
+  articles_page <- read_lines(articles_page_path)
 
-    desc <- '<meta property="og:description" content="The Open Graph protocol is a standard for web page metadata."'
-    img_vig <- '<meta property="og:image" content="http://example.com/pkg/batpig.png"'
-    twitter_creator_vig <- '<meta name="twitter:creator" content="@dataandme"'
-    twitter_card_vig <- '<meta name="twitter:card" content="summary"'
+  desc <- '<meta property="og:description" content="The Open Graph protocol is a standard for web page metadata."'
+  img_vig <- '<meta property="og:image" content="http://example.com/pkg/batpig.png"'
+  twitter_creator_vig <- '<meta name="twitter:creator" content="@dataandme"'
+  twitter_card_vig <- '<meta name="twitter:card" content="summary"'
 
-    expect_equal(n_matches(articles_page, desc), 1)
-    expect_equal(n_matches(articles_page, img_vig), 1)
-    expect_equal(n_matches(articles_page, img_alt), 1)
-    expect_equal(n_matches(articles_page, twitter_creator_vig), 1)
-    expect_equal(n_matches(articles_page, twitter_site), 1)
-    expect_equal(n_matches(articles_page, twitter_card_vig), 1)
-  })
+  expect_equal(n_matches(articles_page, desc), 1)
+  expect_equal(n_matches(articles_page, img_vig), 1)
+  expect_equal(n_matches(articles_page, img_alt), 1)
+  expect_equal(n_matches(articles_page, twitter_creator_vig), 1)
+  expect_equal(n_matches(articles_page, twitter_site), 1)
+  expect_equal(n_matches(articles_page, twitter_card_vig), 1)
 })
 
 og_exp <- list(
