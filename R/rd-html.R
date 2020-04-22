r-lib/pkgdown

commit 015f5f56d842a45094beda9bb8d40b2b8f79fc9b

Tweak dl styling (#1266)
* Tweak dl styling

Fixes #786

* Update test

R/rd-html.R
@@ -329,7 +329,7 @@ as_html.tag_enumerate <- function(x, ...) {
 }
 #' @export
 as_html.tag_describe <- function(x, ...) {
-  paste0("<dl class='dl-horizontal'>\n", parse_descriptions(x[-1], ...), "\n</dl>")
+  paste0("<dl'>\n", parse_descriptions(x[-1], ...), "\n</dl>")
 }
 
 # Effectively does nothing: only used by parse_items() to split up

R/test.R
@@ -25,9 +25,24 @@
 #'
 #' \subsection{Definition list}{
 #' \describe{
-#'   \item{a}{1}
-#'   \item{b}{2}
-#'   \item{This is a very long definition term}{}
+#'   \item{short}{short}
+#'   \item{short}{Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+#'   eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+#'   minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+#'   ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+#'   voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+#'   sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+#'   mollit anim id est laborum.}
+#'   \item{Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+#'   eiusmod tempor incididunt ut labore et dolore magna aliqua.}{short}
+#'   \item{Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+#'   eiusmod tempor incididunt ut labore et dolore magna aliqua.}{Lorem ipsum
+#'   adipiscing elit, sed do  eiusmod tempor incididunt ut labore et dolore ad
+#'   minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+#'   ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+#'   voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+#'   sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+#'   mollit anim id est laborum.}
 #' }
 #' }
 #' @keywords internal

inst/assets/pkgdown.css
@@ -71,6 +71,10 @@ summary {
   margin-top: calc(-60px + 1em);
 }
 
+dd {
+  margin-left: 3em;
+}
+
 /* Section anchors ---------------------------------*/
 
 a.anchor {

man/test-lists.Rd
@@ -26,9 +26,24 @@
 
 \subsection{Definition list}{
 \describe{
-  \item{a}{1}
-  \item{b}{2}
-  \item{This is a very long definition term}{}
+  \item{short}{short}
+  \item{short}{Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+  minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+  ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+  voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+  sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+  mollit anim id est laborum.}
+  \item{Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+  eiusmod tempor incididunt ut labore et dolore magna aliqua.}{short}
+  \item{Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+  eiusmod tempor incididunt ut labore et dolore magna aliqua.}{Lorem ipsum
+  adipiscing elit, sed do  eiusmod tempor incididunt ut labore et dolore ad
+  minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+  ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+  voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+  sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+  mollit anim id est laborum.}
 }
 }
 }

tests/testthat/test-rd-html.R
@@ -317,7 +317,7 @@ test_that("nested item with whitespace parsed correctly", {
       This text is indented in a way pkgdown doesn't like.
   }}")
   expect_equal(out, c(
-    "<dl class='dl-horizontal'>",
+    "<dl'>",
     "<dt>Label</dt><dd><p>This text is indented in a way pkgdown doesn't like.</p></dd>",
     "</dl>"
   ))

commit cd0d9d2a57c635e95c0a383f19f10085ebe02e6c

Fix typo in <dl> header
015f5f56d842a45094beda9bb8d40b2b8f79fc9b introduced a typo into the `<dl>` header, which causes subsequent blocks to be absorbed into the `<dl>` by some rendering engines (e.g. Chrome’s). In most (all?) cases this luckily only causes subtle spacing issues.

R/rd-html.R
@@ -329,7 +329,7 @@ as_html.tag_enumerate <- function(x, ...) {
 }
 #' @export
 as_html.tag_describe <- function(x, ...) {
-  paste0("<dl'>\n", parse_descriptions(x[-1], ...), "\n</dl>")
+  paste0("<dl>\n", parse_descriptions(x[-1], ...), "\n</dl>")
 }
 
 # Effectively does nothing: only used by parse_items() to split up