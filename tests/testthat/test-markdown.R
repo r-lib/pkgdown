ea65bb0ef1658c1dfa457b4a234ffa40fb08326c
Jonathan Keane <jkeane@gmail.com>
2020-08-20 18:36:36 -0500

    move tests to the appropriate file

--- tests/testthat/test-markdown.R
+++ tests/testthat/test-markdown.R
@@ -1,3 +1,10 @@
 test_that("empty string works", {
   expect_equal(markdown_text(""), "")
 })
+
+test_that("header attributes are parsed", {
+  index_xml <- markdown(normalizePath(test_path("assets/readme-with-attr.md")))
+
+  expect_true(grepl("id=\"id\"", index_xml))
+  expect_true(grepl("class=\".*? class\"", index_xml))
+})
