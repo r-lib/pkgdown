5f1ef9785827a976afbdc09a2d2a626d6ef1f2b4
Maëlle Salmon <maelle.salmon@yahoo.se>
2021-03-28 15:05:37 +0200

    keep logo at the same height by adding padding (#1589)

--- R/render.r
+++ R/render.r
@@ -27,10 +27,7 @@ render_page <- function(pkg = ".", name, data, path = "", depth = NULL, quiet = 
     depth <- length(strsplit(path, "/")[[1]]) - 1L
   }
 
-  data$logo <- list(
-    src = logo_path(pkg, depth = depth),
-    id = if (is.null(data$source)) "logo-low" else "logo-high"
-  )
+  data$logo <- list(src = logo_path(pkg, depth = depth))
 
   data <- utils::modifyList(data, data_template(pkg, depth = depth))
   data$pkgdown <- list(
