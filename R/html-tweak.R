eede29e5932fa42bbb588cacb0c8bb24f354a911
Maëlle Salmon <maelle.salmon@yahoo.se>
2021-04-06 16:45:10 +0200

    no

--- R/html-tweak.R
+++ R/html-tweak.R
@@ -369,7 +369,7 @@ activate_navbar <- function(html, path, pkg) {
   }
 
   hrefs <- lapply(nav_items, get_hrefs, pkg = pkg)
-  hrefs <- do.call(rbind, hrefs, args = list(make.row.names = FALSE))
+  hrefs <- do.call(rbind, hrefs)
   path <- remove_useless_parts(path, pkg = pkg)
 
   separate_path <- function(link) {
