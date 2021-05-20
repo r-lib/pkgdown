771657cfbd0e4f5c021fc5e3df0685fc0116acef
Maëlle Salmon <maelle.salmon@yahoo.se>
2021-05-20 15:38:28 +0200

    the order matters!

--- R/render.r
+++ R/render.r
@@ -423,18 +423,19 @@ data_deps <- function(pkg, depth) {
     )
   )
 
-  pkgdown_sass <- path_pkgdown("css", paste0("BS", bs_version), "pkgdown.sass")
-  code_sass <- path_pkgdown("css", paste0("BS", bs_version), "syntax-highlighting.sass")
-  all_sass <- paste(c(read_lines(pkgdown_sass), read_lines(code_sass)), collapse = "")
-  pkgdown_css <- sass::sass_partial(all_sass, bs_theme)
-  bs_theme <- bslib::bs_add_rules(bs_theme, pkgdown_css)
-
   # map secondary to component-active-bg
   secondary <- bslib::bs_get_variables(bs_theme, "secondary")
   bs_theme <- bslib::bs_add_variables(
     bs_theme,
     "component-active-bg" = as.character(secondary)
   )
+
+  # pkgdown sass
+  pkgdown_sass <- path_pkgdown("css", paste0("BS", bs_version), "pkgdown.sass")
+  code_sass <- path_pkgdown("css", paste0("BS", bs_version), "syntax-highlighting.sass")
+  all_sass <- paste(c(read_lines(pkgdown_sass), read_lines(code_sass)), collapse = "")
+  pkgdown_css <- sass::sass_partial(all_sass, bs_theme)
+  bs_theme <- bslib::bs_add_rules(bs_theme, pkgdown_css)
 
   deps <- bslib::bs_theme_dependencies(bs_theme, sass::sass_options_get(output_style = "expanded"))
   # Add other dependencies - TODO: more of those?
