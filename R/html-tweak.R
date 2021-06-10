9ce6af43b55cd21e9563526e12a12d1243657bf6
Maëlle Salmon <maelle.salmon@yahoo.se>
2021-06-10 08:32:53 +0200

    Remove sidebar TOC when useless (#1711)

--- R/html-tweak.R
+++ R/html-tweak.R
@@ -375,6 +375,28 @@ navbar_links_haystack <- function(html, pkg, path) {
   haystack[haystack$similar > 0, ]
 }
 
+trim_toc <- function(html) {
+  if (count_heading(html) <= 0) {
+    xml2::xml_remove(xml2::xml_find_first(html, '//nav[@id="toc"]'))
+  }
+}
+
+count_heading <- function(html) {
+  # - 1 to remove one for the contents header :-)
+  sum(purrr::map_dbl(2:6, count_heading_level, html)) - 1
+}
+
+count_heading_level <- function(level, html) {
+  length(
+    xml2::xml_find_all(
+      xml2::xml_find_first(html, "//body"),
+      # exclude dropdown headers
+      sprintf("//h%s[not(contains(@class, 'dropdown'))]", level)
+    )
+  )
+}
+
+
 tweak_404 <- function(html, pkg = pkg) {
 
   # If there's no URL links can't be made absolute
