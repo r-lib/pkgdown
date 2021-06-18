51268b0d9cb0588d7a6404a2b8ef4ee19c435e89
Maëlle Salmon <maelle.salmon@yahoo.se>
2021-06-18 14:31:13 +0200

    support for tabs in BS4 (#1694)
    Co-authored-by: Hadley Wickham <h.wickham@gmail.com>

--- tests/testthat/test-html-tweak.R
+++ tests/testthat/test-html-tweak.R
@@ -354,3 +354,69 @@ test_that("activate_navbar()", {
       xml2::xml_find_first(navbar, ".//li[contains(@class, 'active')]")
   )
 })
+
+# tabsets -------------------------------------------------------------
+
+test_that("tweak_tabsets() default", {
+  html <- '<div id="results-in-tabset" class="section level2 tabset">
+<h2 class="hasAnchor">
+<a href="#results-in-tabset" class="anchor" aria-hidden="true"></a>Results in tabset</h2>
+<div id="tab-1" class="section level3">
+<h3 class="hasAnchor">
+<a href="#tab-1" class="anchor" aria-hidden="true"></a>Tab 1</h3>
+<p>blablablabla</p>
+<div class="sourceCode" id="cb9"><pre class="downlit sourceCode r">
+<code class="sourceCode R"><span class="fl">1</span> <span class="op">+</span> <span class="fl">1</span></code></pre></div>
+</div>
+<div id="tab-2" class="section level3">
+<h3 class="hasAnchor">
+<a href="#tab-2" class="anchor" aria-hidden="true"></a>Tab 2</h3>
+<p>blop</p>
+</div>
+</div>'
+  new_html <- tweak_tabsets(xml2::read_html(html))
+  expect_snapshot_output(cat(as.character(new_html)))
+})
+
+test_that("tweak_tabsets() with tab pills and second tab active", {
+  html <- '<div id="results-in-tabset" class="section level2 tabset tabset-pills">
+<h2 class="hasAnchor">
+<a href="#results-in-tabset" class="anchor" aria-hidden="true"></a>Results in tabset</h2>
+<div id="tab-1" class="section level3">
+<h3 class="hasAnchor">
+<a href="#tab-1" class="anchor" aria-hidden="true"></a>Tab 1</h3>
+<p>blablablabla</p>
+<div class="sourceCode" id="cb9"><pre class="downlit sourceCode r">
+<code class="sourceCode R"><span class="fl">1</span> <span class="op">+</span> <span class="fl">1</span></code></pre></div>
+</div>
+<div id="tab-2" class="section level3 active">
+<h3 class="hasAnchor">
+<a href="#tab-2" class="anchor" aria-hidden="true"></a>Tab 2</h3>
+<p>blop</p>
+</div>
+</div>'
+  new_html <- tweak_tabsets(xml2::read_html(html))
+  expect_snapshot_output(cat(as.character(new_html)))
+})
+
+
+test_that("tweak_tabsets() with tab pills, fade and second tab active", {
+  html <- '<div id="results-in-tabset" class="section level2 tabset tabset-pills tabset-fade">
+<h2 class="hasAnchor">
+<a href="#results-in-tabset" class="anchor" aria-hidden="true"></a>Results in tabset</h2>
+<div id="tab-1" class="section level3">
+<h3 class="hasAnchor">
+<a href="#tab-1" class="anchor" aria-hidden="true"></a>Tab 1</h3>
+<p>blablablabla</p>
+<div class="sourceCode" id="cb9"><pre class="downlit sourceCode r">
+<code class="sourceCode R"><span class="fl">1</span> <span class="op">+</span> <span class="fl">1</span></code></pre></div>
+</div>
+<div id="tab-2" class="section level3 active">
+<h3 class="hasAnchor">
+<a href="#tab-2" class="anchor" aria-hidden="true"></a>Tab 2</h3>
+<p>blop</p>
+</div>
+</div>'
+  new_html <- tweak_tabsets(xml2::read_html(html))
+  expect_snapshot_output(cat(as.character(new_html)))
+})
