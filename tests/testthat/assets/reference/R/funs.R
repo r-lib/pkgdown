d37e91c4390386233960ad7f424a9cde86a6d968
Duncan Murdoch <murdoch.duncan@gmail.com>
2021-07-29 07:50:34 -0400

    Add test for dependencies.

--- tests/testthat/assets/reference/R/funs.R
+++ tests/testthat/assets/reference/R/funs.R
@@ -15,3 +15,33 @@ c <- function() {}
 #' \special{?topic}
 #' @export
 `?` <- function() {}
+
+#' E
+#' @examples
+#' pkgdown_print.foo <- function(x, visible) x
+#' registerS3method("pkgdown_print","foo", pkgdown_print.foo,
+#'                  envir = asNamespace("pkgdown"))
+#' replay_html.foo <- function(x, ...)
+#' structure(
+#'   "<div>test</div>\n",
+#'   html = TRUE,
+#'   class = base::c("html",
+#'             "character"),
+#'   dependencies = list(
+#'     structure(
+#'       list(
+#'         name = "foo",
+#'         version = "1.0",
+#'         src = list(file = system.file("javascript", package = "testpackage")),
+#'         script = "foo.js",
+#'         all_files = TRUE
+#'       ),
+#'       class = "html_dependency"
+#'     )
+#'   )
+#' )
+#' registerS3method("replay_html", "foo", replay_html.foo,
+#'   envir = asNamespace("downlit"))
+#' structure("text", class = "foo")
+#'
+e <- function() {}
--- tests/testthat/test-build-reference.R
+++ tests/testthat/test-build-reference.R
@@ -7,3 +7,14 @@ test_that("parse failures include file name", {
     build_reference(test_path("assets/reference-fail"))
   )
 })
+
+test_that("dependencies are included in reference output", {
+  withr::defer(
+    unlink(test_path("assets/reference/docs"), recursive = TRUE)
+  )
+
+  build_reference(test_path("assets/reference"), topic = "e")
+  lines <- read_lines(test_path("assets/reference/docs/reference/e.html"))
+  expect_true(any(grepl("^<!-- dependencies from examples -->$", lines)))
+  expect_true(any(grepl("<script src=\"libs/foo-1.0/foo.js\"></script>", lines)))
+})
