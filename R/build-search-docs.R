85d8beb537a6cf3c050438398d610da03dba3353
Maëlle Salmon <maelle.salmon@yahoo.se>
2021-09-20 08:54:24 +0200

    Add mention of pkgdown_sitrep() in build_search() docs (#1771)

--- R/build-search-docs.R
+++ R/build-search-docs.R
@@ -75,10 +75,15 @@ url_node <- function(url) {
 #' search:
 #'   exclude: ['news/index.html']
 #' ```
-#' @section Local testing:
-#' Search won't work if you simply use pkgdown preview of the static files.
+#' @section Debugging and local testing:
+#'
+#' Locally (as opposed to on GitHub Pages or Netlify for instance),
+#' search won't work if you simply use pkgdown preview of the static files.
 #' You can use `servr::httw("docs")` instead.
 #'
+#' If search is not working, run `pkgdown::pkgdown_sitrep()` to eliminate
+#' common issues such as the absence of URL in the pkgdown configuration file
+#' of your package.
 #'
 #' @inheritParams build_articles
 #' @export
