ca041a613451e8585b9fb245e872ed7a6ba435cc
Hadley Wickham <h.wickham@gmail.com>
2021-09-22 13:03:33 -0500

    Restore htmlwidget dependencies
    And test, so we don't accidentally break again!

--- tests/testthat/test-highlight.R
+++ tests/testthat/test-highlight.R
@@ -1,3 +1,25 @@
+test_that("highlight_examples captures depencies", {
+  withr::defer(unlink("Rplot001.png"))
+
+  # TODO: remove once https://github.com/r-lib/downlit/pull/110 is merged/used
+  if (packageVersion("downlit") < "0.2.9000.9001") {
+    registerS3method("replay_html", "htmlwidget", envir = asNamespace("downlit"),
+      function(x, ...) {
+        rendered <- htmltools::renderTags(x)
+        structure(rendered$html, dependencies = rendered$dependencies)
+      }
+    )
+  }
+
+  dummy_dep <- htmltools::htmlDependency("dummy", "1.0.0", "dummy.js")
+  widget <- htmlwidgets::createWidget("test", list(), dependencies = dummy_dep)
+  out <- highlight_examples("widget", env = environment())
+
+  # htmlwidgets always get dependency on htmlwidgets.js
+  expect_equal(attr(out, "dependencies")[-1], list(dummy_dep))
+})
+
+
 test_that("pre() can produce needed range of outputs", {
   expect_snapshot({
     cat(pre("x"))
