df6a199c13afcb31fc3df8a66f8457110e00de06
Hadley Wickham <h.wickham@gmail.com>
2021-09-23 14:28:49 -0500

    Update for dev downlit

--- tests/testthat/test-highlight.R
+++ tests/testthat/test-highlight.R
@@ -1,16 +1,6 @@
 test_that("highlight_examples captures depencies", {
   withr::defer(unlink("Rplot001.png"))
 
-  # TODO: remove once https://github.com/r-lib/downlit/pull/110 is merged/used
-  if (packageVersion("downlit") < "0.2.9000.9001") {
-    registerS3method("replay_html", "htmlwidget", envir = asNamespace("downlit"),
-      function(x, ...) {
-        rendered <- htmltools::renderTags(x)
-        structure(rendered$html, dependencies = rendered$dependencies)
-      }
-    )
-  }
-
   dummy_dep <- htmltools::htmlDependency("dummy", "1.0.0", "dummy.js")
   widget <- htmlwidgets::createWidget("test", list(), dependencies = dummy_dep)
   out <- highlight_examples("widget", env = environment())
