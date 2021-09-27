6fc0ec35211cb415eedb7f40245531a483a713b9
Hadley Wickham <h.wickham@gmail.com>
2021-09-27 11:23:29 -0500

    Move output-html-vignette to a test

--- tests/testthat/test-build-articles.R
+++ tests/testthat/test-build-articles.R
@@ -4,3 +4,11 @@ test_that("can recognise intro variants", {
   expect_true(article_is_intro("pack-age", "pack.age"))
   expect_true(article_is_intro("articles/pack-age", "pack.age"))
 })
+
+test_that("can build article that uses html_vignette", {
+  pkg <- as_pkgdown(test_path("assets/article-html-vignette"))
+  withr::defer(clean_site(pkg))
+
+  expect_output(expect_error(build_article("html-vignette", pkg), NA))
+})
+
