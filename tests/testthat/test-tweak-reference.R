6245c82389200a6effe9926e63a7da68b12363db
Hadley Wickham <h.wickham@gmail.com>
2021-10-12 17:46:30 -0500

    Convert snapshots to equality tests

--- tests/testthat/test-tweak-reference.R
+++ tests/testthat/test-tweak-reference.R
@@ -2,12 +2,13 @@ test_that("tweak_reference_topic_html() works", {
   html <- xml2::read_html('
     <div class="ref-section">
       <div class="sourceCode r">
-      <pre><code>1 + 1</code></pre>
+      <pre><code>1 + 2</code></pre>
       </div>
     </div>
   ')
   tweak_reference_topic_html(html)
-  expect_snapshot_output(xpath_xml(html, "//pre"))
+  expect_equal(xpath_attr(html, "//code/span", "class"), c("fl", "op", "fl"))
+  expect_equal(xpath_text(html, "//code/span"), c("1", "+", "2"))
 
   html <- xml2::read_html('
     <div class="ref-section">
@@ -17,18 +18,21 @@ test_that("tweak_reference_topic_html() works", {
     </div>
   ')
   tweak_reference_topic_html(html)
-  expect_snapshot_output(xpath_xml(html, "//pre"))
+  # pandoc adds an outer span for each line
+  expect_equal(xpath_attr(html, "//code/span/span", "class"), c("fu", "kw", "at"))
+  expect_equal(xpath_text(html, "//code/span/span"), c("field", ":", " value"))
 })
 
 test_that("works with unlabelled pre", {
   # If parseable, assume R
   html <- xml2::read_html('
     <div class="ref-section">
-      <pre><code>foo()</code></pre>
+      <pre><code>1 + 2</code></pre>
     <div>
   ')
   tweak_reference_topic_html(html)
-  expect_snapshot_output(xpath_xml(html, "//code"))
+  expect_equal(xpath_attr(html, "//code/span", "class"), c("fl", "op", "fl"))
+  expect_equal(xpath_text(html, "//code/span"), c("1", "+", "2"))
 
   # If not parseable, leave as is
   html <- xml2::read_html('
@@ -37,5 +41,5 @@ test_that("works with unlabelled pre", {
     <div>
   ')
   tweak_reference_topic_html(html)
-  expect_snapshot_output(xpath_xml(html, "//code"))
+  expect_equal(xpath_length(html, "//code//span"), 0)
 })
