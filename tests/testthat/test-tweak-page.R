r-lib/pkgdown

commit 284c2bb4f57f408db51fd2c017165d2816493386

Apply HTML tweaks more consistently (#1838)
New tweak_page() applies the standard set of tweaks in render_page(). This means that markdown_*() no longer applies any tweaks so the news tweaking can be simplified.

Fixes #1835

R/build-reference.R
@@ -256,11 +256,6 @@ build_reference_index <- function(pkg = ".") {
     path = "reference/index.html"
   )
 
-  html <- xml2::read_html(file.path(pkg$dst_path, "reference/index.html"), encoding = "UTF-8")
-  tweak_link_external(html, pkg = pkg)
-  tweak_anchors(html)
-  xml2::write_html(html, file.path(pkg$dst_path, "reference/index.html"))
-
   invisible()
 }
 

R/markdown.R
@@ -73,15 +73,10 @@ markdown_path_html <- function(path, strip_header = FALSE, pkg = list()) {
     xml2::xml_remove(h1)
   }
 
-  downlit::downlit_html_node(xml)
-  tweak_link_md(xml)
-  tweak_link_external(xml, pkg = pkg)
-  tweak_anchors(xml)
-
   structure(xml, title = title)
 }
 
-markdown_to_html <- function(text, dedent = 4) {
+markdown_to_html <- function(text, dedent = 4, bs_version = 3) {
   if (dedent) {
     text <- gsub(paste0("($|\n)", strrep(" ", dedent)), "\\1", text, perl = TRUE)
   }
@@ -93,7 +88,7 @@ markdown_to_html <- function(text, dedent = 4) {
   convert_markdown_to_html(md_path, html_path)
 
   html <- xml2::read_html(html_path, encoding = "UTF-8")
-  tweak_anchors(html)
+  tweak_page(html, list(bs_version = bs_version))
   html
 }
 

R/render.R
@@ -71,13 +71,10 @@ render_page <- function(pkg = ".", name, data, path = "", depth = NULL, quiet = 
   rendered <- render_template(template, components)
 
   html <- xml2::read_html(rendered, encoding = "UTF-8")
+
+  tweak_page(html, pkg = pkg)
   if (pkg$bs_version > 3) {
-    tweak_footnotes(html)
     activate_navbar(html, data$output_file %||% path, pkg)
-    trim_toc(html)
-  }
-  if (pkg$desc$has_dep("R6")) {
-    tweak_link_R6(html, pkg$package)
   }
   for (tweak in tweaks) {
     tweak(html)

R/tweak-page.R
@@ -1,17 +1,25 @@
 # File level tweaks --------------------------------------------
-
-tweak_rmarkdown_html <- function(html, input_path, pkg = pkg) {
+tweak_page <- function(html, pkg = list()) {
   # Automatically link function mentions
   downlit::downlit_html_node(html)
   tweak_anchors(html)
   tweak_link_md(html)
   tweak_link_external(html, pkg = pkg)
+  tweak_tables(html)
+  tweak_img_src(html)
 
   if (pkg$bs_version > 3) {
     tweak_footnotes(html)
     tweak_tabsets(html)
+    trim_toc(html)
   }
 
+  if (!is.null(pkg$desc) && pkg$desc$has_dep("R6")) {
+    tweak_link_R6(html, pkg$package)
+  }
+}
+
+tweak_rmarkdown_html <- function(html, input_path, pkg = pkg) {
   # Tweak classes of navbar
   toc <- xml2::xml_find_all(html, ".//div[@id='tocnav']//ul")
   xml2::xml_attr(toc, "class") <- "nav nav-pills nav-stacked"
@@ -29,8 +37,10 @@ tweak_rmarkdown_html <- function(html, input_path, pkg = pkg) {
     )
   }
 
-  tweak_img_src(html)
-  tweak_tables(html)
+  # Has to occur after path normalisation
+  # This get called twice on the contents of content-article.html, but that
+  # should be harmless
+  tweak_page(html, pkg = pkg)
 
   invisible()
 }
@@ -80,9 +90,6 @@ tweak_homepage_html <- function(html,
         .where = "before"
       )
   }
-
-  tweak_img_src(html)
-  tweak_tables(html)
 
   invisible()
 }

tests/testthat/test-tweak-page.R
@@ -16,7 +16,7 @@ test_that("links to vignettes & figures tweaked", {
     <img src="../man/figures/x.png" />
   </body>')
 
-  tweak_homepage_html(html, bs_version = 3)
+  tweak_page(html, list(bs_version = 3, desc = desc::desc(text = "")))
   expect_equal(
     xpath_attr(html, ".//img", "src"),
     c("articles/x.png", "../articles/x.png", "reference/figures/x.png", "../reference/figures/x.png")

tests/testthat/test-rmarkdown.R
@@ -15,7 +15,7 @@ test_that("render_rmarkdown copies image files in subdirectories", {
 test_that("render_rmarkdown yields useful error", {
   skip_if_no_pandoc()
   tmp <- dir_create(file_temp())
-  pkg <- list(src_path = test_path("."), dst_path = tmp)
+  pkg <- list(src_path = test_path("."), dst_path = tmp, bs_version = 3)
 
   expect_snapshot(error = TRUE, {
     render_rmarkdown(pkg, "assets/pandoc-fail.Rmd", "test.html")

commit 5150236bd3e87be9abc6e43a8a804b707bf1fdf2

Further centralise tweak_page()
By passing `name` to `tweak_page()` we can apply page based logic in one place. This allows us to fix #1846, and remove the tweaks argument from `render_page()` since it's no longer needed.

I also fixed the duplication of the `external-link` class since it was also broken in #1838.

R/build-reference.R
@@ -311,8 +311,7 @@ build_reference_topic <- function(topic,
   render_page(
     pkg, "reference-topic",
     data = data,
-    path = path("reference", topic$file_out),
-    tweaks = list(tweak_reference_highlighting)
+    path = path("reference", topic$file_out)
   )
 
   invisible()

R/markdown.R
@@ -88,7 +88,7 @@ markdown_to_html <- function(text, dedent = 4, bs_version = 3) {
   convert_markdown_to_html(md_path, html_path)
 
   html <- xml2::read_html(html_path, encoding = "UTF-8")
-  tweak_page(html, list(bs_version = bs_version))
+  tweak_page(html, "markdown", list(bs_version = bs_version))
   html
 }
 

R/render.R
@@ -19,9 +19,8 @@
 #'   If `""` (the default), prints to standard out.
 #' @param depth Depth of path relative to base directory.
 #' @param quiet If `quiet`, will suppress output messages
-#' @param tweaks List of "tweak" functions applied to output HTML.
 #' @export
-render_page <- function(pkg = ".", name, data, path = "", depth = NULL, quiet = FALSE, tweaks = NULL) {
+render_page <- function(pkg = ".", name, data, path = "", depth = NULL, quiet = FALSE) {
   pkg <- as_pkgdown(pkg)
 
   if (is.null(depth)) {
@@ -68,12 +67,9 @@ render_page <- function(pkg = ".", name, data, path = "", depth = NULL, quiet = 
 
   html <- xml2::read_html(rendered, encoding = "UTF-8")
 
-  tweak_page(html, pkg = pkg)
+  tweak_page(html, name, pkg = pkg)
   if (pkg$bs_version > 3) {
     activate_navbar(html, data$output_file %||% path, pkg)
-  }
-  for (tweak in tweaks) {
-    tweak(html)
   }
 
   rendered <- as.character(html, options = character())

R/tweak-page.R
@@ -1,12 +1,23 @@
 # File level tweaks --------------------------------------------
-tweak_page <- function(html, pkg = list()) {
-  # Automatically link function mentions
-  downlit::downlit_html_node(html)
+tweak_page <- function(html, name, pkg = list()) {
+  # Syntax highlighting and linking
+  if (name == "reference-topic") {
+    # Reference topic takes a minimal approach since some is
+    # already handled by Rd processing
+    tweak_reference_highlighting(html)
+  } else {
+    downlit::downlit_html_node(html)
+  }
+
   tweak_anchors(html)
   tweak_link_md(html)
   tweak_link_external(html, pkg = pkg)
-  tweak_tables(html)
   tweak_img_src(html)
+
+  # BS3 uses table for layout of reference-index
+  if (name != "reference-index") {
+    tweak_tables(html)
+  }
 
   if (pkg$bs_version > 3) {
     tweak_footnotes(html)
@@ -40,7 +51,7 @@ tweak_rmarkdown_html <- function(html, input_path, pkg = pkg) {
   # Has to occur after path normalisation
   # This get called twice on the contents of content-article.html, but that
   # should be harmless
-  tweak_page(html, pkg = pkg)
+  tweak_page(html, "article", pkg = pkg)
 
   invisible()
 }

R/tweak-tags.R
@@ -67,9 +67,13 @@ tweak_link_external <- function(html, pkg = list()) {
   if (length(links) == 0)
     return()
 
+  links <- links[!has_class(links, "external-link")]
+
   hrefs <- xml2::xml_attr(links, "href")
+  links <- links[!is_internal_link(hrefs, pkg = pkg)]
+
   # Users might have added absolute URLs to e.g. the Code of Conduct
-  tweak_class_prepend(links[!is_internal_link(hrefs, pkg = pkg)], "external-link")
+  tweak_class_prepend(links, "external-link")
 
   invisible()
 }
@@ -128,14 +132,7 @@ tweak_link_R6 <- function(html, cur_package) {
 }
 
 tweak_tables <- function(html) {
-  # Don't tweak reference index since the BS3 reference index uses a table
-  # which is styled differently.
-  div <- xml2::xml_find_first(html, ".//div[contains(@class, 'template-reference-index')]")
-  if (!is.na(div)) {
-    return()
-  }
-
-  # Ensure all tables have class="table"
+  # Ensure all tables have class="table" apart from arguments
   table <- xml2::xml_find_all(html, ".//table")
   table <- table[!has_class(table, "ref-arguments")]
 

man/render_page.Rd
@@ -5,15 +5,7 @@
 \alias{data_template}
 \title{Render page with template}
 \usage{
-render_page(
-  pkg = ".",
-  name,
-  data,
-  path = "",
-  depth = NULL,
-  quiet = FALSE,
-  tweaks = NULL
-)
+render_page(pkg = ".", name, data, path = "", depth = NULL, quiet = FALSE)
 
 data_template(pkg = ".", depth = 0L)
 }
@@ -39,8 +31,6 @@ If \code{""} (the default), prints to standard out.}
 \item{depth}{Depth of path relative to base directory.}
 
 \item{quiet}{If \code{quiet}, will suppress output messages}
-
-\item{tweaks}{List of "tweak" functions applied to output HTML.}
 }
 \description{
 Each page is composed of four templates: "head", "header", "content", and

tests/testthat/test-tweak-page.R
@@ -16,10 +16,21 @@ test_that("links to vignettes & figures tweaked", {
     <img src="../man/figures/x.png" />
   </body>')
 
-  tweak_page(html, list(bs_version = 3, desc = desc::desc(text = "")))
+  tweak_page(html, "article", list(bs_version = 3, desc = desc::desc(text = "")))
   expect_equal(
     xpath_attr(html, ".//img", "src"),
     c("articles/x.png", "../articles/x.png", "reference/figures/x.png", "../reference/figures/x.png")
   )
 })
 
+
+test_that("reference index table is not altered", {
+  html <- xml2::read_html("<body>
+    <div class='template-reference-index'>
+      <table></table>
+    </div>
+  </body>")
+  pkg <- list(bs_version = 3, desc = desc::desc(text = ""))
+  tweak_page(html, "reference-index", pkg)
+  expect_equal(xpath_attr(html, ".//table", "class"), NA_character_)
+})

tests/testthat/test-tweak-tags.R
@@ -16,15 +16,7 @@ test_that("tables get additional table class", {
   )
 })
 
-test_that("except on the reference index or argument list", {
-  html <- xml2::read_html("<body>
-    <div class='template-reference-index'>
-      <table></table>
-    </div>
-  </body>")
-  tweak_tables(html)
-  expect_equal(xpath_attr(html, ".//table", "class"), NA_character_)
-
+test_that("except in the argument list", {
   html <- xml2::read_html("<body>
     <div class='template-reference-index'>
       <table class='ref-arguments'></table>
@@ -134,11 +126,12 @@ test_that("local md links are replaced with html", {
   )
 })
 
-test_that("tweak_link_external() add the external-link class", {
+test_that("tweak_link_external() add the external-link class if needed", {
   html <- xml2::read_html('
     <a href="#anchor"></a>
     <a href="http://remote.com/remote.md"></a>
-    <a class = "thumbnail" href="http://remote.com/remote.md"></a>
+    <a href="http://remote.com/remote.md" class="external-link"></a>
+    <a href="http://remote.com/remote.md" class="thumbnail" ></a>
     <a href="http://example.com/remote.md"></a>
   ')
 
@@ -147,7 +140,7 @@ test_that("tweak_link_external() add the external-link class", {
 
   expect_equal(
     xpath_attr(html, "//a", "class"),
-    c(NA, "external-link", "external-link thumbnail", NA)
+    c(NA, "external-link", "external-link", "external-link thumbnail", NA)
   )
 })
 