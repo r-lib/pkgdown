305880c3666ae26ae97427ebd1973d93b7d868f0
Hadley Wickham <h.wickham@gmail.com>
2021-10-20 07:15:33 -0500

    Fix typo in variable name

--- R/templates.R
+++ R/templates.R
@@ -47,7 +47,7 @@ templates_dir <- function(pkg = list()) {
   if (!is.null(template$path)) {
     # Directory specified in yaml doesn't exist, so eagerly error
     if (!dir_exists(template$path)) {
-      abort(paste0("Can not find templates path ", src_path(template_path)))
+      abort(paste0("Can not find templates path ", src_path(template$path)))
     }
     path_abs(template$path, start = pkg$src_path)
   } else if (!is.null(template$package)) {
