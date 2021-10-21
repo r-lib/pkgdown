9473bf47789a3c32d5f8256449fd173ab15a8549
Hadley Wickham <h.wickham@gmail.com>
2021-10-21 17:15:05 -0500

    Experimenting with TOC

--- R/tweak-page.R
+++ R/tweak-page.R
@@ -102,7 +102,7 @@ tweak_homepage_html <- function(html,
 
 # Strip off #toc if it's not needed; easier to do this here than in js
 tweak_useless_toc <- function(html) {
-  contents <- xml2::xml_find_all(html, ".//div[@id='contents']")
+  contents <- xml2::xml_find_all(html, ".//div[@id='container']")
   headings <- xml2::xml_find_all(contents, ".//h2|.//h3|.//h4|.//h5|.//h6")
 
   if (length(headings) <= 1) {
