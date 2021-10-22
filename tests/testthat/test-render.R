becb88d28327bd4468cb91f710edcd61afbeb914
Hadley Wickham <h.wickham@gmail.com>
2021-10-22 11:30:41 -0500

    Specify yaml directly in local_pkgdown_site()

--- tests/testthat/test-render.R
+++ tests/testthat/test-render.R
@@ -4,3 +4,32 @@ test_that("check_bootswatch_theme() works", {
   expect_snapshot_error(check_bootswatch_theme("paper", 4, list()))
 })
 
+test_that("can include text in header, before body, and after body", {
+  pkg <- local_pkgdown_site(test_path("assets/site-empty"), '
+    template:
+      includes:
+        in_header: <test>in header</test>
+        before_body: <test>before body</test>
+        after_body: <test>after body</test>
+  ')
+
+  expect_named(
+    data_template(pkg)$includes,
+    c("in_header", "before_body", "after_body")
+  )
+
+  pkg$bs_version <- 3
+  html <- render_page_html(pkg, "title-body")
+  expect_equal(
+    xpath_text(html, ".//test"),
+    c("in header", "before body", "after body")
+  )
+
+  pkg$bs_version <- 5
+  expect_output(init_site(pkg))
+  html <- render_page_html(pkg, "title-body")
+  expect_equal(
+    xpath_text(html, ".//test"),
+    c("in header", "before body", "after body")
+  )
+})
