c454846ac8a0f8f72a0d7751191fd103b868784e
Nic Crane <thisisnic@gmail.com>
2022-04-19 15:52:22 +0100

    Remove redirector func, update examples

--- inst/version-selector/version-selector.js
+++ inst/version-selector/version-selector.js
@@ -7,29 +7,6 @@ $(window).bind("pageshow", function(event) {
   }
 });
 
-/**
- * Check if the page to be browsed to exists and if not, redirect to base path
- */
-function check_page_exists_and_redirect(event) {
-
-  const urls = event.target.value.split(",")
-  const path_to_try = urls[0];
-  const fallback_path = urls[1]
-
-  $.ajax({
-      type: 'HEAD',
-      url: path_to_try,
-      success: function() {
-          location.href = path_to_try;
-      }
-  }).fail(function() {
-      location.href = fallback_path;
-  });
-  return false;
-}
-
-
-
 $(document).ready(function () {
 
 /**
@@ -40,7 +17,7 @@ $(document).ready(function () {
   // Load JSON file mapping between docs version and R package version
   $.getJSON(window.location.origin + "/versions.json", function( data ) {
     // get the current page's version number:
-    var displayed_version = $('.version').text();
+    var displayed_version = $('.version').text().trim();
     // Create a dropdown selector and add the appropriate attributes
     const sel = document.createElement("select");
     sel.name = "version-selector";
@@ -48,18 +25,16 @@ $(document).ready(function () {
     sel.classList.add("navbar-default");
     // When the selected value is changed, take the user to the version
     // of the page they are browsing in the selected version
-    sel.onchange = check_page_exists_and_redirect;
+    sel.onchange = function(){
+     if (this.value) window.location.href=this.value;
+    }
 
     // For each of the items in the JSON object (name/version pairs)
     $.each( data, function( key, val ) {
       // Add a new option to the dropdown selector
       const opt = document.createElement("option");
-      // Get the final component of the path
-      const pathEnd = window.location.pathname.replace(val.path, "");
-      // Set the path based on the 'path' field
-      const base_path = window.location.origin + val.path;
       // Set value to the path and the fallback path
-      opt.value = [base_path + pathEnd, base_path];
+      opt.value = window.location.origin + val.path;
       // Set the currently selected item based on the major and minor version numbers
       opt.selected = val.version === displayed_version;
       // Set the displayed text based on the 'label' field
