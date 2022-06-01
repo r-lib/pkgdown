d7921c7e63a201291101188bc25fb614de22c32e
Hadley Wickham <h.wickham@gmail.com>
2022-06-01 11:13:00 -0500

    Add test

--- tests/testthat/test-pkgdown_print.R
+++ tests/testthat/test-pkgdown_print.R
@@ -15,3 +15,12 @@ test_that("htmlwidgets get sized", {
   expect_equal(value$width, 700)
   expect_equal(value$height, 700)
 })
+
+test_that("respect htmlwidgets width", {
+  local_context_eval(list(fig.width = 7, dpi = 100, fig.asp = 1))
+
+  widget <- htmlwidgets::createWidget("test", list(), width = "100px")
+  value <- pkgdown_print(widget)
+
+  expect_equal(value$width, "100px")
+})
