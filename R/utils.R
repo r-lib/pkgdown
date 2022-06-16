ec355060a53deed16b4cc8f91b71925ce59f0350
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-06-16 15:39:13 +0200

    this internal copy is also not needed

--- R/utils.R
+++ R/utils.R
@@ -96,12 +96,6 @@ has_internet <- function() {
   return(getOption("pkgdown.internet", default = TRUE))
 }
 
-with_dir <- function(new, code) {
-  old <- setwd(dir = new)
-  on.exit(setwd(old))
-  force(code)
-}
-
 # remove '' quoting
 # e.g. 'title' becomes title.s
 cran_unquote <- function(string) {
