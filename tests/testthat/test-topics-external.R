090c8c75dd2b0232786a5103ddd1c0a1b72e06a2
Hadley Wickham <h.wickham@gmail.com>
2022-06-16 09:21:39 -0500

    Make sure column names match

--- tests/testthat/test-topics-external.R
+++ tests/testthat/test-topics-external.R
@@ -1,5 +1,9 @@
 test_that("can get info about external function", {
   expect_snapshot(str(ext_topics("base::mean")))
+
+  # and column names match
+  pkg <- as_pkgdown(test_path("assets/reference"))
+  expect_equal(names(ext_topics("base::mean")), names(pkg$topics))
 })
 
 test_that("fails if documentation not available", {
