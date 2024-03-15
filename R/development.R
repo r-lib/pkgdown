2337dd97af4c10d3a084c5e74b52b29a4e4d3f17
olivroy <olivierroy71@hotmail.com>
2024-03-15 15:21:26 -0400

    tweak this message

--- R/development.R
+++ R/development.R
@@ -58,9 +58,9 @@ dev_mode_auto <- function(version) {
 check_mode <- function(mode) {
   valid_mode <- c("auto", "default", "release", "devel", "unreleased")
   if (!mode %in% valid_mode) {
-    abort(paste0(
-      "`development.mode` in `_pkgdown.yml` must be one of ",
-      paste(valid_mode, collapse = ", ")
+    cli::cli_abort(c(
+      "{.field development.mode} must be one of {.or {valid_mode}}.",
+      i = "Use an approriate value in {.run [_pkgdown.yml](usethis::edit_pkgdown_config())}"
     ))
   }
 }
