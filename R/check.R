eba8eece66a546f25b41f87d438b0b5e33c1c521
Hadley Wickham <h.wickham@gmail.com>
2024-04-18 08:01:08 -0500

    Describe what check_pkgdown() actually does (#2460)
    Fixes #2260

--- R/check.R
+++ R/check.R
@@ -1,7 +1,14 @@
 #' Check `_pkgdown.yml`
 #'
+#' @description
 #' Check that your `_pkgdown.yml` is valid without building the whole
-#' site.
+#' site. Currently this:
+#'
+#' * Checks the reference and article indexes to ensure that pkgdown can
+#'   read them, and that every documentation topic and vignette/article is 
+#'   included in the index.
+#'
+#' * Validates any opengraph metadata that you might have supplied
 #'
 #' @export
 #' @inheritParams as_pkgdown
