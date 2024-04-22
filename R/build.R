10a5848d5be89f53373e0c9fc674dec1e9e0f920
Hadley Wickham <h.wickham@gmail.com>
2024-04-22 09:54:06 -0500

    Elimiate redirects config fragment
    Only used in one place

--- R/build.R
+++ R/build.R
@@ -275,8 +275,25 @@
 #'    ```
 #'
 #' @section Redirects:
-#' ```{r child="man/rmd-fragments/redirects-configuration.Rmd"}
+#' If you change the structure of your documentation (by renaming vignettes or 
+#' help topics) you can setup redirects from the old content to the new content.
+#' One or several now-absent pages can be redirected to a new page (or to a new 
+#' section of a new page). This works by creating a html page that performs a 
+#' "meta refresh", which isn't the best way of doing a redirect but works 
+#' everywhere that you might deploy your site.
+#' 
+#' The syntax is the following, with old paths on the left, and new paths or 
+#' URLs on the right.
+#' 
+#' ```yaml
+#' redirects:
+#'   - ["articles/old-vignette-name.html", "articles/new-vignette-name.html"]
+#'   - ["articles/another-old-vignette-name.html", "articles/new-vignette-name.html"]
+#'   - ["articles/yet-another-old-vignette-name.html", "https://pkgdown.r-lib.org/dev"]
 #' ```
+#' 
+#' If for some reason you choose to redirect an existing page make sure to 
+#' exclude it from the search index, see `?build_search`.
 #'
 #' @section Options:
 #' Users with limited internet connectivity can disable CRAN checks by setting
