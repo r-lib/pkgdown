056a2b0ca31b1d35f3dfbe2de5ba5bb475271c2b
Hadley Wickham <h.wickham@gmail.com>
2024-04-22 09:25:43 -0500

    Fix broken test

--- tests/testthat/test-check.R
+++ tests/testthat/test-check.R
@@ -14,7 +14,7 @@ test_that("fails if article index incomplete", {
   pkg <- local_pkgdown_site(test_path("assets/articles"), meta = "
     articles:
      - title: Title
-       contents: [starts_with('html'), random, standard, toc-false, widget]
+       contents: [starts_with('html'), random, standard, toc-false, widget, needs-escape]
   ")
   expect_snapshot(check_pkgdown(pkg), error = TRUE)
 })
