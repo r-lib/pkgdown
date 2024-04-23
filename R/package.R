6689ee1e278b72716c6e8dde43498a9d7c6e0e7b
Hadley Wickham <h.wickham@gmail.com>
2024-04-23 08:22:00 -0500

    Clearer error if _pkgdown.yml missing

--- R/package.R
+++ R/package.R
@@ -192,7 +192,10 @@ pkgdown_config_path <- function(path) {
   )
 }
 pkgdown_config_href <- function(path) {
-  config <- pkgdown_config_path(path) %||% "_pkgdown.yml"
+  config <- pkgdown_config_path(path)
+  if (is.null(config)) {
+    cli::cli_abort("Can't find {.file _pkgdown.yml}.", .internal = TRUE)
+  }
   cli::style_hyperlink(fs::path_file(config), paste0("file://", config))
 }
 
