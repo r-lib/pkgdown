fb6d768f3529c1df8515749e7a4ed767aa5c3c91
Hadley Wickham <h.wickham@gmail.com>
2024-05-14 14:59:16 -0500

    Deprecate BS3

--- R/check.R
+++ R/check.R
@@ -35,6 +35,13 @@ pkgdown_sitrep <- function(pkg = ".") {
   cli::cli_rule("Sitrep")
 
   pkg <- as_pkgdown(pkg)
+  if (pkg$bs_version == 3) {
+    cli::cli_inform(c(
+      x = "Bootstrap 3 is deprecated; please switch to Bootstrap 5.",
+      i = "Learn more at {.url https://www.tidyverse.org/blog/2021/12/pkgdown-2-0-0/#bootstrap-5}."
+    ))
+  }
+
   error_to_sitrep("URLs", check_urls(pkg))
   error_to_sitrep("Open graph metadata", data_open_graph(pkg))
   error_to_sitrep("Articles metadata", data_articles_index(pkg))
