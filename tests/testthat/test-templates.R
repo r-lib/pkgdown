7e2cc9c19c171db9a37323c0ae6fd0eaab4b50f6
Hadley Wickham <h.wickham@gmail.com>
2024-05-16 10:29:35 -0400

    Fix last linting issue

--- tests/testthat/test-templates.R
+++ tests/testthat/test-templates.R
@@ -73,11 +73,9 @@ test_that("find templates in local pkgdown first", {
 # Expected contents -------------------------------------------------------
 
 test_that("BS5 templates have main + aside", {
-  names <- dir(path_pkgdown("BS5", "templates"), pattern = "content-")
+  names <- dir_ls(path_pkgdown("BS5", "templates"), regexp = "content-")
   names <- path_ext_remove(path_file(names))
   names <- gsub("content-", "", names)
-
-  expect_length(names, 12)
 
   templates <- lapply(names, read_template_html,
     type = "content",
