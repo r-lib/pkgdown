b63cfd87d3d54c2e22a15e4df2e73d016b547d69
Hadley Wickham <h.wickham@gmail.com>
2024-05-22 13:14:01 -0500

    Update test

--- tests/testthat/test-theme.R
+++ tests/testthat/test-theme.R
@@ -1,12 +1,14 @@
 test_that("validations yaml specification", {
   build_bslib_ <- function(...) {
-    pkg <- local_pkgdown_site(meta = list(...))
+    pkg <- local_pkgdown_site(
+      meta = list(template = list(..., bootstrap = 5, `light-switch` = TRUE))
+    )
     build_bslib(pkg)
   }
 
   expect_snapshot(error = TRUE, {
-    build_bslib_(template = list(theme = 1, bootstrap = 5))
-    build_bslib_(template = list(theme = "fruit", bootstrap = 5))
-    build_bslib_(template = list(`theme-dark` = "fruit", bootstrap = 5))
+    build_bslib_(theme = 1)
+    build_bslib_(theme = "fruit")
+    build_bslib_(`theme-dark` = "fruit")
   })
 })
