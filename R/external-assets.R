319fb45102b242970b6067f0a7f6888582634cb0
Hadley Wickham <h.wickham@gmail.com>
2024-05-28 07:47:37 -0500

    Break up into smaller functions and add some tests

--- R/external-assets.R
+++ R/external-assets.R
@@ -3,36 +3,10 @@ assemble_ext_assets <- function(pkg) {
   deps_ext <- yaml::read_yaml(path_assets_yaml)
 
   purrr::map_chr(deps_ext, ~ {
-    # download external resource
     path <- path_deps(pkg, basename(.x$url))
     download.file(.x$url, path, quiet = TRUE, mode = "wb")
+    check_integrity(path, .x$integrity)
 
-    # check file integrity
-    sha_size <- as.integer(regmatches(
-      .x$integrity,
-      regexpr("(?<=^sha)\\d{3}", .x$integrity, perl = TRUE)
-    ))
-    if (!(sha_size %in% c(256L, 384L, 512L))) {
-      cli::cli_abort(paste0(
-        "Invalid {.field integrity} value set in {.file ",
-        "{path_assets_yaml}}: {.val {(.x$integrity)}} Allowed are only ",
-        "SHA-256, SHA-384 and SHA-512."
-      ))
-    }
-    con <- file(path, encoding = "UTF-8")
-    hash <- openssl::base64_encode(openssl::sha2(con, sha_size))
-    hash_target <- regmatches(
-      .x$integrity,
-      regexpr("(?<=^sha\\d{3}-).+", .x$integrity, perl = TRUE)
-    )
-
-    if (hash != hash_target) {
-      cli::cli_abort(paste0(
-        "Hash of downloaded {(.x$type)} asset doesn't match {.field ",
-        "integrity} value of {.val {(.x$integrity)}}. Asset URL is: {.url ",
-        "{(.x$url)}}"
-      ))
-    }
     .x$url <- fs::path_rel(path, pkg$dst_path)
 
     # assemble HTML tag
@@ -43,4 +17,36 @@ assemble_ext_assets <- function(pkg) {
       cli::cli_abort("Unknown asset type {.val {.x$type}} defined in {.file path_assets_yaml}.")
     )
   })
+}
+
+check_integrity <- function(path, integrity) {
+  parsed <- parse_integrity(integrity)
+  if (!parsed$size %in% c(256L, 384L, 512L)) {
+    cli::cli_abort(
+      "{.field integrity} must use SHA-256, SHA-384, or SHA-512",
+      .internal = TRUE
+    )
+  }
+
+  hash <- compute_hash(path, parsed$size)
+  if (hash != parsed$hash) {
+    cli::cli_abort(
+      "Downloaded asset does not match known integrity",
+      .internal = TRUE
+    )
+  }
+
+  invisible()
+}
+
+compute_hash <- function(path, size) {
+  con <- file(path, encoding = "UTF-8")
+  openssl::base64_encode(openssl::sha2(con, size))
+}
+ 
+parse_integrity <- function(x) {
+  size <- as.integer(regmatches(x, regexpr("(?<=^sha)\\d{3}", x, perl = TRUE)))
+  hash <- regmatches(x, regexpr("(?<=^sha\\d{3}-).+", x, perl = TRUE))
+
+  list(size = size, hash = hash)
 }
