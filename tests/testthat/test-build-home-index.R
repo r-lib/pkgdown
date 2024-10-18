8d9cba19fe597bba170f8d4c99f351491ab4f4d1
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-10-18 03:11:10 +0200

    Suppress warning from {desc} in test

--- tests/testthat/test-build-home-index.R
+++ tests/testthat/test-build-home-index.R
@@ -156,7 +156,7 @@ test_that("cran_unquote works", {
 
 test_that("allow email in BugReports", {
   # currently desc throws a warning if BugReports is an email
-  pkg <- local_pkgdown_site(desc = list(BugReports = "me@tidyverse.com"))
+  pkg <- suppressWarnings(local_pkgdown_site(desc = list(BugReports = "me@tidyverse.com")))
   html <- xml2::read_html(data_home_sidebar(pkg))
   expect_snapshot(xpath_xml(html, ".//li/a"))
 })
