b883843530ce16a2e2e1df3f863c5ee104445a04
Hadley Wickham <h.wickham@gmail.com>
2025-10-07 07:35:10 -0500

    style tweaks

--- R/build-llm.R
+++ R/build-llm.R
@@ -1,13 +1,10 @@
 build_llm_docs <- function(pkg = ".") {
+  rlang::check_installed("pandoc")
   pkg <- as_pkgdown(pkg)
 
   cli::cli_rule("Building llm documentation")
-  if (pkg$development$in_dev && pkg$bs_version > 3) {
-    url <- paste0(url, pkg$prefix)
-  }
-
   paths <- get_site_paths(pkg)
-  purrr::walk(paths, build_md, pkg = pkg)
+  purrr::walk(paths, convert_md, pkg = pkg)
 
   index <- c(
     read_file_if_exists(path(pkg$dst_path, "index.html.md")),
@@ -19,45 +16,33 @@ build_llm_docs <- function(pkg = ".") {
   invisible()
 }
 
-read_file_if_exists <- function(path) {
-  if (file_exists(path)) {
-    read_lines(path)
-  }
-}
-
-build_md <- function(path, pkg) {
-  rlang::check_installed("pandoc")
+convert_md <- function(path, pkg) {
   path <- path(pkg[["dst_path"]], path)
 
-  main_html <- xml2::read_html(path) |>
-    xml2::xml_find_first(".//main")
-
+  html <- xml2::read_html(path)
+  main_html <- xml2::xml_find_first(html, ".//main")
   if (length(main_html) == 0) {
     return()
   }
 
-  # uninformative image (logo) + source link
+  # simplify page header (which includes logo + source link)
   title <- xml2::xml_find_first(main_html, ".//h1")
   xml2::xml_remove(
     xml2::xml_find_first(main_html, ".//div[@class='page-header']")
   )
   xml2::xml_add_child(main_html, title, .where = 0)
 
-  # clean links
+  # drop anchors
   xml2::xml_remove(xml2::xml_find_all(main_html, ".//a[@class='anchor']"))
 
-  internal_links <- xml2::xml_find_all(
-    main_html,
-    ".//a[not(@class='external-link')]"
-  )
-
+  # replace all internal links with absolute link to .md
+  internal <- xml2::xml_find_all(main_html, ".//a[not(@class='external-link')]")
   purrr::walk(
-    internal_links,
+    internal,
     add_website_url,
     pkg = pkg,
     root = fs::path_rel(fs::path_dir(path), start = pkg$dst_path)
   )
-
   xml2::xml_set_attr(
     xml2::xml_find_all(main_html, ".//a[@class='external-link']"),
     attr = "class",
@@ -71,6 +56,8 @@ build_md <- function(path, pkg) {
     output = sprintf("%s.md", path)
   )
 }
+
+# Helpers ---------------------------------------------------------------------
 
 add_website_url <- function(node, pkg, root) {
   url <- sprintf("%s/", config_pluck_string(pkg, "url"))
@@ -87,4 +74,11 @@ add_website_url <- function(node, pkg, root) {
     attr = "href",
     value = sprintf("%s%s.md", url, xml2::xml_attr(node, "href"))
   )
+}
+
+
+read_file_if_exists <- function(path) {
+  if (file_exists(path)) {
+    read_lines(path)
+  }
 }
