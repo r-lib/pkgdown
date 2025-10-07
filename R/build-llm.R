46642ecc89c2bd97dac4634ccd6f8a6287a8af71
Hadley Wickham <h.wickham@gmail.com>
2025-10-07 07:29:14 -0500

    Simplify llms.txt generation

--- R/build-llm.R
+++ R/build-llm.R
@@ -1,6 +1,5 @@
 build_llm_docs <- function(pkg = ".") {
   pkg <- as_pkgdown(pkg)
-
   if (refuses_mds(pkg)) {
     return()
   }
@@ -10,55 +9,32 @@ build_llm_docs <- function(pkg = ".") {
     return()
   }
 
-  cli::cli_rule("Building Markdowns")
+  cli::cli_rule("Building llm documentation")
   if (pkg$development$in_dev && pkg$bs_version > 3) {
     url <- paste0(url, pkg$prefix)
   }
 
   paths <- get_site_paths(pkg)
-
   purrr::walk(paths, build_md, pkg = pkg)
 
-  readme_path <- path(pkg[["dst_path"]], "index.html.md")
-  if (file_exists(readme_path)) {
-    readme <- read_lines(readme_path) |>
-      paste(collapse = "\n")
-  } else {
-    readme <- ""
-  }
-
-  reference_path <- path(pkg[["dst_path"]], "reference", "index.html.md")
-  if (file_exists(reference_path)) {
-    reference <- read_lines(reference_path) |>
-      paste(collapse = "\n")
-  } else {
-    reference <- ""
-  }
-
-  article_path <- path(pkg[["dst_path"]], "articles", "index.html.md")
-  if (file_exists(article_path)) {
-    articles <- read_lines(article_path) |>
-      paste(collapse = "\n")
-  } else {
-    articles <- ""
-  }
-
-  llms_lines <- whisker::whisker.render(
-    read_file(find_template("summary", "llms", ext = ".txt", pkg = pkg)),
-    data = list(
-      readme = readme,
-      reference = reference,
-      articles = articles
-    )
+  index <- c(
+    read_file_if_exists(path(pkg$dst_path, "index.html.md")),
+    read_file_if_exists(path(pkg$dst_path, "reference", "index.html.md")),
+    read_file_if_exists(path(pkg$dst_path, "articles", "index.html.md"))
   )
-
-  write_lines(llms_lines, path(pkg[["dst_path"]], "llms.txt"))
+  writeLines(index, path(pkg$dst_path, "llms.txt"))
 
   invisible()
 }
 
 refuses_mds <- function(pkg) {
   !config_pluck_bool(pkg, "template.build-md", default = TRUE)
+}
+
+read_file_if_exists <- function(path) {
+  if (file_exists(path)) {
+    read_lines(path)
+  }
 }
 
 build_md <- function(path, pkg) {
@@ -77,17 +53,10 @@ build_md <- function(path, pkg) {
   xml2::xml_remove(
     xml2::xml_find_first(main_html, ".//div[@class='page-header']")
   )
-
-  xml2::xml_add_child(
-    main_html,
-    title,
-    .where = 0
-  )
+  xml2::xml_add_child(main_html, title, .where = 0)
 
   # clean links
-  xml2::xml_remove(
-    xml2::xml_find_all(main_html, ".//a[@class='anchor']")
-  )
+  xml2::xml_remove(xml2::xml_find_all(main_html, ".//a[@class='anchor']"))
 
   internal_links <- xml2::xml_find_all(
     main_html,
