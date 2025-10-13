6eb626a19bbd50f1dd65826bb227b65446080169
Maëlle Salmon <maelle.salmon@yahoo.se>
2025-10-13 12:31:46 +0200

    footnote

--- R/build-llm.R
+++ R/build-llm.R
@@ -36,6 +36,9 @@ convert_md <- function(path, pkg) {
     xml2::xml_add_child(main_html, title, .where = 0)
   }
 
+  # fix footnotes
+  convert_popovers_to_footnotes(main_html)
+
   # drop internal anchors
   xml2::xml_remove(xml2::xml_find_all(main_html, ".//a[@class='anchor']"))
 
@@ -73,4 +76,79 @@ create_absolute_links <- function(main_html, pkg) {
     href_absolute <- path_ext_set(href_absolute, "md")
   }
   xml2::xml_attr(a, "class") <- NULL
+}
+
+convert_popovers_to_footnotes <- function(main_html) {
+  # Find all popover footnote references
+  popover_refs <- xml2::xml_find_all(
+    main_html,
+    ".//a[@class='footnote-ref'][@data-bs-content]"
+  )
+
+  if (length(popover_refs) == 0) {
+    return(main_html)
+  }
+
+  # Create footnotes section if it doesn't exist
+  footnotes_section <- xml2::xml_find_first(
+    main_html,
+    ".//section[@class='footnotes']"
+  )
+  if (length(footnotes_section) == 0) {
+    # Add footnotes section at the end of main
+    footnotes_section <- xml2::xml_add_child(
+      main_html,
+      "section",
+      id = "footnotes",
+      class = "footnotes footnotes-end-of-document",
+      role = "doc-endnotes"
+    )
+    xml2::xml_add_child(footnotes_section, "hr")
+    footnotes_ol <- xml2::xml_add_child(footnotes_section, "ol")
+  } else {
+    footnotes_ol <- xml2::xml_find_first(footnotes_section, ".//ol")
+  }
+
+  # Process each popover reference using purrr
+  purrr::iwalk(popover_refs, function(ref, i) {
+    # Extract footnote content from data-bs-content
+    content <- xml2::xml_attr(ref, "data-bs-content")
+
+    # Decode HTML entities in the content
+    content <- xml2::xml_text(xml2::read_html(paste0(
+      "<div>",
+      content,
+      "</div>"
+    )))
+
+    # Create footnote ID
+    fn_id <- paste0("fn", i)
+    fnref_id <- paste0("fnref", i)
+
+    # Update the reference link
+    xml2::xml_attr(ref, "href") <- paste0("#", fn_id)
+    xml2::xml_attr(ref, "class") <- "footnote-ref"
+    xml2::xml_attr(ref, "id") <- fnref_id
+    xml2::xml_attr(ref, "role") <- "doc-noteref"
+    xml2::xml_set_attr(ref, "tabindex", NULL)
+    xml2::xml_set_attr(ref, "data-bs-toggle", NULL)
+    xml2::xml_set_attr(ref, "data-bs-content", NULL)
+
+    # Create footnote list item
+    fn_li <- xml2::xml_add_child(footnotes_ol, "li", id = fn_id)
+    fn_p <- xml2::xml_add_child(fn_li, "p")
+    xml2::xml_text(fn_p) <- content
+
+    # Add back reference
+    back_ref <- xml2::xml_add_child(
+      fn_p,
+      "a",
+      "↩︎",
+      href = paste0("#", fnref_id),
+      class = "footnote-back",
+      role = "doc-backlink"
+    )
+  })
+
+  return(main_html)
 }
