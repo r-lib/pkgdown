fb8444951141b8faa7e7018a62b774a9582617b8
Jay Hesselberth <jay.hesselberth@gmail.com>
2025-10-17 16:13:39 -0600

    Update todos

--- R/pkgdown-package.R
+++ R/pkgdown-package.R
@@ -12,7 +12,9 @@ NULL
 release_bullets <- function() {
   c(
     "Check that [test/widget.html](https://pkgdown.r-lib.org/dev/articles/) responds to mouse clicks on 5/10/50",
-    "Compile language files with `potools::po_compile()`",
-    "Update `vignette/translations.Rmd` with new languages"
+    "Update translations with `potools::po_extract()` + `potools::po_update()`",
+    "Use an LLM to proofread / fill in missing translations (see #2926)",
+    "Compile po files with `potools::po_compile()`",
+    "Update `vignette/translations.Rmd` with any new languages"
   )
 }
