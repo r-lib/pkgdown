6b9a0a3a0d123dcba9b44d6bad267aeb11939e09
Jay Hesselberth <jay.hesselberth@gmail.com>
2025-10-17 17:21:02 -0600

    Add release bullets for translations (#2933)
    Closes #2927

--- R/pkgdown-package.R
+++ R/pkgdown-package.R
@@ -11,6 +11,10 @@ NULL
 
 release_bullets <- function() {
   c(
-    "Check that [test/widget.html](https://pkgdown.r-lib.org/dev/articles/) responds to mouse clicks on 5/10/50"
+    "Check that [test/widget.html](https://pkgdown.r-lib.org/dev/articles/) responds to mouse clicks on 5/10/50",
+    "Update translations with `potools::po_extract()` + `potools::po_update()`",
+    "Use an LLM to proofread / fill in missing translations (see #2926)",
+    "Compile po files with `potools::po_compile()`",
+    "Update `vignette/translations.Rmd` with any new languages"
   )
 }
