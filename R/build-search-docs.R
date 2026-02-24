0abd9580ed191208bb0b1a8dce2fc3338a11cea3
shikokuchuo <53399081+shikokuchuo@users.noreply.github.com>
2026-02-24 13:13:20 +0000

    Update search docs section

--- R/build-search-docs.R
+++ R/build-search-docs.R
@@ -64,11 +64,7 @@ build_sitemap <- function(pkg = ".") {
 #' search:
 #'   exclude: ['news/index.html']
 #' ```
-#' # Debugging and local testing
-#'
-#' Locally (as opposed to on GitHub Pages or Netlify for instance),
-#' search won't work if you simply use pkgdown preview of the static files.
-#' You can use `servr::httw("docs")` instead.
+#' # Debugging
 #'
 #' If search is not working, run `pkgdown::pkgdown_sitrep()` to eliminate
 #' common issues such as the absence of URL in the pkgdown configuration file
